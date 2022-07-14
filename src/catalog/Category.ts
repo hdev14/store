3da8419d36a8e8f6fdd32dbe7a2b7158876e2bbc
Hermerson Araújo <ne.hermerson@gmail.com>
2022-07-14 20:50:53 -0300

    feat(catalog): start Category entity

--- src/catalog/Category.ts
+++ src/catalog/Category.ts
@@ -1,12 +1,35 @@
 import uuid from 'uuid';
 import Entity from '../shared/abstractions/Entity';
+import EntityValidator from '../shared/utils/EntityValidator';
 
 export default class Category extends Entity {
-  constructor() {
+  private _name: string;
+
+  private _code: number;
+
+  constructor(name: string, code: number) {
     super(uuid.v4());
+    this._name = name;
+    this._code = code;
   }
 
-  validate(): boolean {
-    return !!this;
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `${this._name} - ${this._code}`;
+  }
+
+  validate(): void {
+    EntityValidator
+      .setData(this.toObject())
+      .setRule('name', ['required'])
+      .setRule('code', ['required', 'min:1'])
+      .validate();
   }
 }
