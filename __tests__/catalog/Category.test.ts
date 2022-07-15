442844542c6b66607c50cc3fd32176c764816687
Hermerson Araújo <ne.hermerson@gmail.com>
2022-07-15 16:12:18 -0300

    test(catalog): create tests to Category entity

--- __tests__/catalog/Category.test.ts
+++ __tests__/catalog/Category.test.ts
@@ -1,5 +1,22 @@
+import Category from '../../src/catalog/Category';
+import ValidationEntityError from '../../src/shared/errors/ValidationEntityError';
+
 describe("Category's Unit Tests", () => {
-  it('', () => {
-    expect(true).toBe(true);
+  it('doesn\'t throw any error if params are correct', () => {
+    expect(() => new Category({ id: 'testid', name: 'test', code: 1 })).not.toThrow(ValidationEntityError);
+  });
+
+  it('throws a ValidationEntityError if params are invalid', () => {
+    expect(() => new Category({
+      id: 'testid',
+      name: '',
+      code: 1,
+    })).toThrow(ValidationEntityError);
+
+    expect(() => new Category({
+      id: 'testid',
+      name: 'test',
+      code: 0,
+    })).toThrow(ValidationEntityError);
   });
 });
