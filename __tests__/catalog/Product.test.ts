f17696157993a97f8cdefa0134e0501db6868dac
Hermerson Araújo <ne.hermerson@gmail.com>
2022-07-15 16:13:00 -0300

    test(catalog): create initial tests for product entity

--- __tests__/catalog/Product.test.ts
+++ __tests__/catalog/Product.test.ts
@@ -1,5 +1,76 @@
+import Category from '../../src/catalog/Category';
+import Dimensions from '../../src/catalog/Dimensions';
+import Product from '../../src/catalog/Product';
+import ValidationEntityError from '../../src/shared/errors/ValidationEntityError';
+
 describe("Product's Unit Tests", () => {
-  it('', () => {
-    expect(true).toBe(true);
+  it('doesn\'t throw a ValidationEntityError if params are correct', () => {
+    const dimensions = new Dimensions({ height: 1, width: 1, depth: 1 });
+    const category = new Category({ id: 'testid', name: 'test', code: 1 });
+
+    expect(() => new Product({
+      id: 'testid',
+      name: 'test',
+      description: 'test',
+      amount: Math.random() * 100,
+      category,
+      dimensions,
+      createdAt: new Date(),
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+    })).not.toThrow(ValidationEntityError);
+  });
+
+  it('throws a ValidationEntityError if params are invalid', () => {
+    const dimensions = new Dimensions({ height: 1, width: 1, depth: 1 });
+    const category = new Category({ id: 'testid', name: 'test', code: 1 });
+
+    expect(() => new Product({
+      id: 'testid',
+      name: '',
+      description: 'test',
+      amount: Math.random() * 100,
+      createdAt: new Date(),
+      dimensions,
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+      category,
+    })).toThrow(ValidationEntityError);
+
+    expect(() => new Product({
+      id: 'testid',
+      name: 'test',
+      description: '',
+      amount: Math.random() * 100,
+      createdAt: new Date(),
+      dimensions,
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+      category,
+    })).toThrow(ValidationEntityError);
+
+    expect(() => new Product({
+      id: 'testid',
+      name: 'test',
+      description: 'test',
+      amount: Math.random() * 100,
+      createdAt: new Date(),
+      dimensions,
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+      category: {} as Category,
+    })).toThrow(ValidationEntityError);
+
+    expect(() => new Product({
+      id: 'testid',
+      name: 'test',
+      description: 'test',
+      amount: Math.random() * 100,
+      createdAt: new Date(),
+      dimensions,
+      image: 'invalid',
+      stockQuantity: 10,
+      category,
+    })).toThrow(ValidationEntityError);
   });
 });
