f18ef4fe6cd61bef108f84add7203c33e198b6b2
Hermerson Araújo <ne.hermerson@gmail.com>
2022-07-15 16:35:12 -0300

    test(catalog): create tests to product entity

--- __tests__/catalog/Product.test.ts
+++ __tests__/catalog/Product.test.ts
@@ -73,4 +73,111 @@ describe("Product's Unit Tests", () => {
       category,
     })).toThrow(ValidationEntityError);
   });
+
+  it('actives the product', () => {
+    const product = new Product({
+      id: 'testid',
+      name: 'test',
+      description: 'test',
+      amount: Math.random() * 100,
+      category: new Category({ id: 'testid', name: 'test', code: 1 }),
+      dimensions: new Dimensions({ height: 1, width: 1, depth: 1 }),
+      createdAt: new Date(),
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+    });
+
+    product.activeProduct();
+
+    expect(product.active).toBe(true);
+  });
+
+  it('deactives the product', () => {
+    const product = new Product({
+      id: 'testid',
+      name: 'test',
+      description: 'test',
+      amount: Math.random() * 100,
+      category: new Category({ id: 'testid', name: 'test', code: 1 }),
+      dimensions: new Dimensions({ height: 1, width: 1, depth: 1 }),
+      createdAt: new Date(),
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+    });
+
+    product.deactiveProduct();
+
+    expect(product.active).toBe(false);
+  });
+
+  it('changes the product\'s category', () => {
+    const product = new Product({
+      id: 'testid',
+      name: 'test',
+      description: 'test',
+      amount: Math.random() * 100,
+      category: new Category({ id: 'categoryid1', name: 'test', code: 1 }),
+      dimensions: new Dimensions({ height: 1, width: 1, depth: 1 }),
+      createdAt: new Date(),
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+    });
+
+    product.changeCategory(new Category({ id: 'categoryid2', name: 'test', code: 2 }));
+
+    expect(product.category.id).toBe('categoryid2');
+  });
+
+  it('removes product\'s quantity from stock ', () => {
+    const product = new Product({
+      id: 'testid',
+      name: 'test',
+      description: 'test',
+      amount: Math.random() * 100,
+      category: new Category({ id: 'testid', name: 'test', code: 1 }),
+      dimensions: new Dimensions({ height: 1, width: 1, depth: 1 }),
+      createdAt: new Date(),
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+    });
+
+    product.removeFromStock(2);
+
+    expect(product.stockQuantity).toBe(8);
+  });
+
+  it('adds product\'s quantity to stock ', () => {
+    const product = new Product({
+      id: 'testid',
+      name: 'test',
+      description: 'test',
+      amount: Math.random() * 100,
+      category: new Category({ id: 'testid', name: 'test', code: 1 }),
+      dimensions: new Dimensions({ height: 1, width: 1, depth: 1 }),
+      createdAt: new Date(),
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+    });
+
+    product.addToStock(2);
+
+    expect(product.stockQuantity).toBe(12);
+  });
+
+  it('checks if has stock for a specific quantity', () => {
+    const product = new Product({
+      id: 'testid',
+      name: 'test',
+      description: 'test',
+      amount: Math.random() * 100,
+      category: new Category({ id: 'testid', name: 'test', code: 1 }),
+      dimensions: new Dimensions({ height: 1, width: 1, depth: 1 }),
+      createdAt: new Date(),
+      image: 'http://example.com/test',
+      stockQuantity: 10,
+    });
+
+    expect(product.hasStockFor(10)).toBe(true);
+    expect(product.hasStockFor(11)).toBe(false);
+  });
 });
