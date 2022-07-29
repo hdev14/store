bbccdcf6c2774ddf969f869aea525afcdf51d97d
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-07-29 20:07:54 -0300

    test(catalog): create test to ensures that ProductService.getAllProducts return the correct products

--- __tests__/catalog/app/ProductService.test.ts
+++ __tests__/catalog/app/ProductService.test.ts
@@ -4,29 +4,50 @@ import { IProductOperations } from '../../../src/catalog/domain/IProductReposito
 import Product from '../../../src/catalog/domain/Product';
 import ProductNotFound from '../../../src/catalog/app/ProductNotFound';
 
-const fakeProducts = [{
-  id: 'test_product_id_1',
-  name: 'test_product_1',
-  description: 'test_product_1',
-  amount: Math.random() * 100,
-  image: 'http://example.com',
-  stockQuantity: parseInt((Math.random() * 10).toFixed(0), 10),
-  createdAt: new Date(),
-  dimensions: {
-    height: Math.random() * 50,
-    width: Math.random() * 50,
-    depth: Math.random() * 50,
+const fakeProducts = [
+  {
+    id: 'test_product_id_1',
+    name: 'test_product_1',
+    description: 'test_product_1',
+    amount: Math.random() * 100,
+    image: 'http://example.com',
+    stockQuantity: parseInt((Math.random() * 10).toFixed(0), 10),
+    createdAt: new Date(),
+    dimensions: {
+      height: Math.random() * 50,
+      width: Math.random() * 50,
+      depth: Math.random() * 50,
+    },
+    category: {
+      id: 'test_category_id_1',
+      name: 'test_category_1',
+      code: 123,
+    },
   },
-  category: {
-    id: 'test_category_id_1',
-    name: 'test_category_1',
-    code: 123,
+  {
+    id: 'test_product_id_2',
+    name: 'test_product_2',
+    description: 'test_product_2',
+    amount: Math.random() * 100,
+    image: 'http://example.com',
+    stockQuantity: parseInt((Math.random() * 10).toFixed(0), 10),
+    createdAt: new Date(),
+    dimensions: {
+      height: Math.random() * 50,
+      width: Math.random() * 50,
+      depth: Math.random() * 50,
+    },
+    category: {
+      id: 'test_category_id_1',
+      name: 'test_category_1',
+      code: 123,
+    },
   },
-}];
+];
 
 class RepositoryStub implements IProductOperations {
   getAllProducts(): Promise<Product[]> {
-    throw new Error('Method not implemented.');
+    return Promise.resolve(fakeProducts as any);
   }
 
   getProductById(id: string): Promise<Product | null> {
@@ -47,14 +68,29 @@ class RepositoryStub implements IProductOperations {
 }
 
 describe('ProductsService\'s unit tests', () => {
-  it('returns a product by id', async () => {
-    expect.assertions(9);
+  it('returns all products', async () => {
+    expect.assertions(2);
 
     const repositoryStub = new RepositoryStub();
-    const productService = new ProductService(repositoryStub);
+    const getAllProductsSpy = jest.spyOn(repositoryStub, 'getAllProducts');
 
+    const productService = new ProductService(repositoryStub);
+    const products = await productService.getAllProducts();
+
+    expect(products).toHaveLength(fakeProducts.length);
+    expect(getAllProductsSpy).toHaveBeenCalled();
+  });
+
+  it('returns a product by id', async () => {
+    expect.assertions(10);
+
+    const repositoryStub = new RepositoryStub();
+    const getProductByIdSpy = jest.spyOn(repositoryStub, 'getProductById');
+
+    const productService = new ProductService(repositoryStub);
     const product = await productService.getProductById('test_product_id_1');
 
+    expect(getProductByIdSpy).toHaveBeenCalled();
     expect(product.id).toEqual(fakeProducts[0].id);
     expect(product.name).toEqual(fakeProducts[0].name);
     expect(product.description).toEqual(fakeProducts[0].description);
@@ -67,9 +103,11 @@ describe('ProductsService\'s unit tests', () => {
   });
 
   it('throws an exception of type ProductNotFound if the product is null', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
 
     const repositoryStub = new RepositoryStub();
+    const getProductByIdSpy = jest.spyOn(repositoryStub, 'getProductById');
+
     const productService = new ProductService(repositoryStub);
 
     try {
@@ -77,6 +115,7 @@ describe('ProductsService\'s unit tests', () => {
     } catch (e: any) {
       expect(e).toBeInstanceOf(ProductNotFound);
       expect(e.message).toEqual('O produto não foi encontrado.');
+      expect(getProductByIdSpy).toHaveBeenCalled();
     }
   });
 });
