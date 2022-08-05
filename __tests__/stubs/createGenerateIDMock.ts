f34a41f730621d2a5f07c720ab563e2c1674f2ac
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-04 21:16:54 -0300

    refactor(test): improve imports

--- __tests__/stubs/createGenerateIDMock.ts
+++ __tests__/stubs/createGenerateIDMock.ts
@@ -1,4 +1,4 @@
-import IGenerateID from '@catalog/app/IGenerateID';
+import IGenerateID from '@shared/utils/IGenerateID';
 
 function createGenerateIDMock(items: any): IGenerateID {
   return jest.fn(() => `test_product_id_${items.length + 1}`);
