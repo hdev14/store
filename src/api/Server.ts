9292fade8df8da81ca2a7b91e56771e64cb75ec0
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-04 21:19:00 -0300

    feat(api): create express server and main entrypoint function

--- src/api/Server.ts
+++ src/api/Server.ts
@@ -1,1 +1,26 @@
-// TODO: create Server class to startup the product.
+import express from 'express';
+import catalog from './routers/catalog';
+
+export class Server {
+  private _application: express.Application;
+
+  constructor() {
+    this._application = express();
+    this.globalMiddlewares();
+    this.routes();
+  }
+
+  get application() {
+    return this._application;
+  }
+
+  private routes() {
+    this._application.use('/catalog', catalog);
+  }
+
+  private globalMiddlewares() {
+    this._application.use(express.json());
+  }
+}
+
+export default new Server();
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,21 @@
-// TODO: main
+/* eslint-disable wrap-iife */
+import Prisma from '@shared/Prisma';
+import Server from './Server';
+
+(async function main() {
+  let connection;
+
+  try {
+    connection = Prisma.connect();
+
+    Server.application.listen(3000, () => {
+      console.info('Server is running at http://localhost:3000');
+    });
+  } catch (e: any) {
+    console.error(e.stack);
+  } finally {
+    if (connection) {
+      await connection.$disconnect();
+    }
+  }
+})();
