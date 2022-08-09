bae3aa9e03aa7c954ca5b9361e42078a5f75e484
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-09 20:54:05 -0300

    chore(test): create global var to db connection

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,15 +1,12 @@
 import Server from '@api/Server';
 import supertest from 'supertest';
 import Prisma from '@shared/Prisma';
-import { PrismaClient } from '@prisma/client';
-
-let connection: PrismaClient;
 
 beforeAll(() => {
   globalThis.request = supertest(Server.application);
-  connection = Prisma.connect();
+  globalThis.dbConnection = Prisma.connect();
 });
 
 afterAll(async () => {
-  await connection.$disconnect();
+  await globalThis.dbConnection.$disconnect();
 });
--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,4 @@
 declare module globalThis {
   declare var request: import('supertest').SuperTest<import('supertest').Test>;
+  declare var dbConnection: import('@prisma/client').PrismaClient;
 }
