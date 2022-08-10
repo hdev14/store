1eb45035b20a64336a7ccf9536fbcb3e7f0e43bb
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-10 20:25:34 -0300

    test(catalog): create test to ensures that CatalogController.getAllProducts works

--- __tests__/api/catalog.int.test.ts
+++ __tests__/api/catalog.int.test.ts
@@ -1,44 +1,44 @@
 import { faker } from '@faker-js/faker';
 
 describe('Catalog\'s Integration Tests', () => {
-  let category: any;
-  let product: any;
-
-  beforeAll(async () => {
-    category = await globalThis.dbConnection.category.create({
-      data: {
-        id: faker.datatype.uuid(),
-        code: parseInt(faker.random.numeric(5), 10),
-        name: faker.word.adjective(),
-      },
-    });
-
-    product = await globalThis.dbConnection.product.create({
-      data: {
-        id: faker.datatype.uuid(),
-        name: faker.commerce.product(),
-        description: faker.commerce.productDescription(),
-        amount: faker.datatype.float(100),
-        active: faker.datatype.boolean(),
-        depth: faker.datatype.number(50),
-        height: faker.datatype.number(50),
-        width: faker.datatype.number(50),
-        stockQuantity: faker.datatype.number(100),
-        image: faker.internet.url(),
-        createdAt: new Date(),
-        categoryId: category.id,
-      },
-    });
-  });
-
-  afterAll(async () => {
-    await globalThis.dbConnection.$transaction([
-      globalThis.dbConnection.product.deleteMany(),
-      globalThis.dbConnection.category.deleteMany(),
-    ]);
-  });
-
   describe('/catalog/products/:id', () => {
+    let category: any;
+    let product: any;
+
+    beforeAll(async () => {
+      category = await globalThis.dbConnection.category.create({
+        data: {
+          id: faker.datatype.uuid(),
+          code: parseInt(faker.random.numeric(5), 10),
+          name: faker.word.adjective(),
+        },
+      });
+
+      product = await globalThis.dbConnection.product.create({
+        data: {
+          id: faker.datatype.uuid(),
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          amount: faker.datatype.float(100),
+          active: faker.datatype.boolean(),
+          depth: faker.datatype.number(50),
+          height: faker.datatype.number(50),
+          width: faker.datatype.number(50),
+          stockQuantity: faker.datatype.number(100),
+          image: faker.internet.url(),
+          createdAt: new Date(),
+          categoryId: category.id,
+        },
+      });
+    });
+
+    afterAll(async () => {
+      await globalThis.dbConnection.$transaction([
+        globalThis.dbConnection.product.deleteMany(),
+        globalThis.dbConnection.category.deleteMany(),
+      ]);
+    });
+
     it("returns a not found response if product doesn't exist", async () => {
       expect.assertions(2);
 
@@ -62,6 +62,87 @@ describe('Catalog\'s Integration Tests', () => {
       expect(response.body.product.id).toEqual(product.id);
       expect(response.body.product.name).toEqual(product.name);
       expect(response.body.product.category.id).toEqual(category.id);
+    });
+  });
+
+  // TODO: test GET: '/catalog/products'
+  describe('/catalog/products', () => {
+    beforeAll(async () => {
+      const category = await globalThis.dbConnection.category.create({
+        data: {
+          id: faker.datatype.uuid(),
+          code: parseInt(faker.random.numeric(5), 10),
+          name: faker.word.adjective(),
+        },
+      });
+
+      const products = [
+        {
+          id: faker.datatype.uuid(),
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          amount: faker.datatype.float(100),
+          active: faker.datatype.boolean(),
+          depth: faker.datatype.number(50),
+          height: faker.datatype.number(50),
+          width: faker.datatype.number(50),
+          stockQuantity: faker.datatype.number(100),
+          image: faker.internet.url(),
+          createdAt: new Date(),
+          categoryId: category.id,
+        },
+        {
+          id: faker.datatype.uuid(),
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          amount: faker.datatype.float(100),
+          active: faker.datatype.boolean(),
+          depth: faker.datatype.number(50),
+          height: faker.datatype.number(50),
+          width: faker.datatype.number(50),
+          stockQuantity: faker.datatype.number(100),
+          image: faker.internet.url(),
+          createdAt: new Date(),
+          categoryId: category.id,
+        },
+        {
+          id: faker.datatype.uuid(),
+          name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          amount: faker.datatype.float(100),
+          active: faker.datatype.boolean(),
+          depth: faker.datatype.number(50),
+          height: faker.datatype.number(50),
+          width: faker.datatype.number(50),
+          stockQuantity: faker.datatype.number(100),
+          image: faker.internet.url(),
+          createdAt: new Date(),
+          categoryId: category.id,
+        },
+      ];
+
+      await globalThis.dbConnection.$transaction(
+        products.map((data) => globalThis.dbConnection.product.create({ data })),
+      );
+    });
+
+    afterAll(async () => {
+      await globalThis.dbConnection.$transaction([
+        globalThis.dbConnection.product.deleteMany(),
+        globalThis.dbConnection.category.deleteMany(),
+      ]);
+    });
+
+    it('returns all product with category data associated', async () => {
+      expect.assertions(3);
+
+      const response = await globalThis.request
+        .get('/catalog/products')
+        .set('Accept', 'application/json');
+
+      expect(response.status).toEqual(200);
+      expect(response.body.results).toHaveLength(3);
+      expect(response.body.results.every((p: any) => !!p.category.id)).toBe(true);
     });
   });
 });
