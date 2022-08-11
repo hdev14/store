5a9ed915900fe21d40b2ff38ae6bd4b90771072a
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-10 22:03:23 -0300

    test(catalog): create test to ensures that CatalogController.updateProductStock removes quantity from produt stock

--- __tests__/api/catalog.int.test.ts
+++ __tests__/api/catalog.int.test.ts
@@ -34,8 +34,8 @@ describe('Catalog\'s Integration Tests', () => {
 
     afterAll(async () => {
       await globalThis.dbConnection.$transaction([
-        globalThis.dbConnection.product.deleteMany(),
-        globalThis.dbConnection.category.deleteMany(),
+        globalThis.dbConnection.product.deleteMany({}),
+        globalThis.dbConnection.category.deleteMany({}),
       ]);
     });
 
@@ -127,8 +127,8 @@ describe('Catalog\'s Integration Tests', () => {
 
     afterAll(async () => {
       await globalThis.dbConnection.$transaction([
-        globalThis.dbConnection.product.deleteMany(),
-        globalThis.dbConnection.category.deleteMany(),
+        globalThis.dbConnection.product.deleteMany({}),
+        globalThis.dbConnection.category.deleteMany({}),
       ]);
     });
 
@@ -216,8 +216,8 @@ describe('Catalog\'s Integration Tests', () => {
 
     afterAll(async () => {
       await globalThis.dbConnection.$transaction([
-        globalThis.dbConnection.product.deleteMany(),
-        globalThis.dbConnection.category.deleteMany(),
+        globalThis.dbConnection.product.deleteMany({}),
+        globalThis.dbConnection.category.deleteMany({}),
       ]);
     });
 
@@ -249,6 +249,65 @@ describe('Catalog\'s Integration Tests', () => {
 
       expect(response.status).toEqual(200);
       expect(response.body.results).toHaveLength(0);
+    });
+  });
+
+  describe('PATCH: /catalog/products/:id/stock', () => {
+    const productId = faker.datatype.uuid();
+    const stockQuantity = parseInt(faker.datatype.number(100).toString(), 10);
+
+    beforeAll(async () => {
+      const category = {
+        id: faker.datatype.uuid(),
+        code: parseInt(faker.random.numeric(5), 10),
+        name: faker.word.adjective(),
+      };
+
+      const product = {
+        id: productId,
+        name: faker.commerce.product(),
+        description: faker.commerce.productDescription(),
+        amount: faker.datatype.float(100),
+        active: faker.datatype.boolean(),
+        depth: faker.datatype.number(50),
+        height: faker.datatype.number(50),
+        width: faker.datatype.number(50),
+        stockQuantity,
+        image: faker.internet.url(),
+        createdAt: new Date(),
+        categoryId: category.id,
+      };
+
+      await globalThis.dbConnection.$transaction([
+        globalThis.dbConnection.category.create({ data: category }),
+        globalThis.dbConnection.product.create({ data: product }),
+      ]);
+    });
+
+    afterAll(async () => {
+      await globalThis.dbConnection.$transaction([
+        globalThis.dbConnection.product.deleteMany({}),
+        globalThis.dbConnection.category.deleteMany({}),
+      ]);
+    });
+
+    it('removes a quantity of product from stock', async () => {
+      expect.assertions(2);
+
+      const qtyToRemove = parseInt((Math.random() * 10).toString(), 10);
+
+      const response = await globalThis.request
+        .patch(`/catalog/products/${productId}/stock`)
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send({ quantity: qtyToRemove * -1 });
+
+      const product = await globalThis.dbConnection.product.findUnique({
+        where: { id: productId },
+      });
+
+      expect(response.status).toEqual(204);
+      expect(product!.stockQuantity).toEqual(stockQuantity - qtyToRemove);
     });
   });
 });
