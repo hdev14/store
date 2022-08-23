757f0fc87c20b98e1ae6fe48e4a580b719030a36
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-23 20:22:15 -0300

    test(catalog): create test to ensures that StockService.removeFromStock calls LowStockProductEvent.send method

--- __tests__/store/catalog/domain/StockService.unit.test.ts
+++ __tests__/store/catalog/domain/StockService.unit.test.ts
@@ -1,6 +1,9 @@
+/* eslint-disable max-classes-per-file */
 import { IProductOperations } from '@catalog/domain/IProductRepository';
 import Product from '@catalog/domain/Product';
 import StockService from '@catalog/domain/StockService';
+import { EventData } from '@shared/@types/events';
+import Event from '@shared/abstractions/Event';
 
 const fakeProduct: any = {
   stockQuantity: 10,
@@ -12,6 +15,17 @@ const fakeProduct: any = {
   },
   hasStockFor: jest.fn(() => true),
 };
+
+class LowStockProductEventStub extends Event {
+  constructor() {
+    super({} as any);
+  }
+
+  send<R>(eventData: EventData): Promise<void | R> {
+    console.info(eventData);
+    return Promise.resolve();
+  }
+}
 
 class ProductRepositoryStub implements IProductOperations {
   getProductsByCategory(_: string): Promise<Product[]> {
@@ -41,7 +55,7 @@ describe('StockService\'s Unit Tests', () => {
       const productRepositoryStub = new ProductRepositoryStub();
       productRepositoryStub.getProductById = jest.fn(() => Promise.resolve(null));
 
-      const stockService = new StockService(productRepositoryStub);
+      const stockService = new StockService(productRepositoryStub, new LowStockProductEventStub());
       const result = await stockService.removeFromStock('test_product_id', 1);
 
       expect(result).toBe(false);
@@ -51,7 +65,7 @@ describe('StockService\'s Unit Tests', () => {
       const productRepositoryStub = new ProductRepositoryStub();
       (fakeProduct.hasStockFor as jest.Mock).mockReturnValueOnce(false);
 
-      const stockService = new StockService(productRepositoryStub);
+      const stockService = new StockService(productRepositoryStub, new LowStockProductEventStub());
       const result = await stockService.removeFromStock('test_product_id', 1);
 
       expect(result).toBe(false);
@@ -62,12 +76,42 @@ describe('StockService\'s Unit Tests', () => {
       productRepositoryStub.updateProduct = jest.fn(() => Promise.resolve({} as Product));
       const removeFromStockSpy = jest.spyOn(fakeProduct, 'removeFromStock');
 
-      const stockService = new StockService(productRepositoryStub);
-      const result = await stockService.removeFromStock('test_product_id', 1);
+      const stockService = new StockService(productRepositoryStub, new LowStockProductEventStub());
+      const result = await stockService.removeFromStock('test_product_id', 5);
 
       expect(result).toBe(true);
-      expect(removeFromStockSpy).toHaveBeenCalledWith(1);
+      expect(removeFromStockSpy).toHaveBeenCalledWith(5);
       expect(productRepositoryStub.updateProduct).toHaveBeenCalledWith(fakeProduct);
+    });
+
+    it('calls LowStockProductEvent.send if stockQuantity is less then 5', async () => {
+      expect.assertions(2);
+
+      const productRepositoryStub = new ProductRepositoryStub();
+      productRepositoryStub.updateProduct = jest.fn(() => Promise.resolve({} as Product));
+      productRepositoryStub.getProductById = jest.fn(() => Promise.resolve({
+        id: 'test_product_id_test',
+        stockQuantity: 10,
+        addToStock(quantity: number) {
+          this.stockQuantity += quantity;
+        },
+        removeFromStock(quantity: number) {
+          this.stockQuantity -= quantity;
+        },
+        hasStockFor: jest.fn(() => true),
+      } as any));
+
+      const lowStockProductEventStub = new LowStockProductEventStub();
+      const sendSpy = jest.spyOn(lowStockProductEventStub, 'send');
+
+      const stockService = new StockService(productRepositoryStub, lowStockProductEventStub);
+      await stockService.removeFromStock('test_product_id_test', 6);
+
+      expect(sendSpy).toHaveBeenCalledTimes(1);
+      expect(sendSpy).toHaveBeenCalledWith({
+        productId: 'test_product_id_test',
+        quantity: 4,
+      });
     });
   });
 
@@ -76,7 +120,7 @@ describe('StockService\'s Unit Tests', () => {
       const productRepositoryStub = new ProductRepositoryStub();
       productRepositoryStub.getProductById = jest.fn(() => Promise.resolve(null));
 
-      const stockService = new StockService(productRepositoryStub);
+      const stockService = new StockService(productRepositoryStub, new LowStockProductEventStub());
       const result = await stockService.addToStock('test_product_id', 1);
 
       expect(result).toBe(false);
@@ -87,7 +131,7 @@ describe('StockService\'s Unit Tests', () => {
       productRepositoryStub.updateProduct = jest.fn(() => Promise.resolve({} as Product));
       const addToStockSpy = jest.spyOn(fakeProduct, 'addToStock');
 
-      const stockService = new StockService(productRepositoryStub);
+      const stockService = new StockService(productRepositoryStub, new LowStockProductEventStub());
       const result = await stockService.addToStock('test_product_id', 1);
 
       expect(result).toBe(true);
