fea7b36660225c9601db280ae51c80d17ee822f9
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-25 20:06:49 -0300

    refactor: change generics

--- src/store/shared/abstractions/Command.ts
+++ src/store/shared/abstractions/Command.ts
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { EventData } from '@shared/@types/events';
 import Event from './Event';
 
-export default abstract class Command extends Event {
-  public abstract validate(): boolean;
+export default abstract class Command<R, T> extends Event<R, T> {
+  public abstract validate(data: EventData<T>): boolean;
 }
--- src/store/shared/abstractions/Event.ts
+++ src/store/shared/abstractions/Event.ts
@@ -1,14 +1,14 @@
 import { EventData } from '@shared/@types/events';
 import EventMediator from './EventMediator';
 
-export default abstract class Event<R = {}, E = {}> {
+export default abstract class Event<R = {}, T = {}> {
   private mediator: EventMediator;
 
   constructor(mediator: EventMediator) {
     this.mediator = mediator;
   }
 
-  public async send(data: EventData<E>): Promise<void | R> {
+  public async send(data: EventData<T>): Promise<void | R> {
     return this.mediator.send<R>(this.constructor.name, data);
   }
 }
