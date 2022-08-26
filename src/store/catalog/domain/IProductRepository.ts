9fa2903cc5111ce11fca3b6ccb74d10c691e011f
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-26 20:17:58 -0300

    refactor(catalog): change type of IProductRepository

--- src/store/catalog/domain/IProductRepository.ts
+++ src/store/catalog/domain/IProductRepository.ts
@@ -3,25 +3,25 @@ import Category from './Category';
 import Product from './Product';
 
 export interface ICategoryOperations {
-  getCategoryById(categoryId: string): Promise<Category | null> | Category | null;
+  getCategoryById(categoryId: string): Promise<Category | null>;
 
   getAllCategories(): Promise<Category[]>;
 
-  addCategory(category: Category): Promise<Category> | Category;
+  addCategory(category: Category): Promise<Category>;
 
-  updateCategory(category: Category): Promise<Category> | Category;
+  updateCategory(category: Category): Promise<Category>;
 }
 
 export interface IProductOperations {
   getAllProducts(): Promise<Product[]>;
 
-  getProductById(id: string): Promise<Product | null> | Product | null;
+  getProductById(id: string): Promise<Product | null>;
 
   getProductsByCategory(categoryId: string): Promise<Product[]>;
 
-  addProduct(product: Product): Promise<Product> | Product;
+  addProduct(product: Product): Promise<Product>;
 
-  updateProduct(product: Product): Promise<Product> | Product;
+  updateProduct(product: Product): Promise<Product>;
 }
 
 interface IProductRepository extends IProductOperations, ICategoryOperations { }
