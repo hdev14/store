20a8a83887f19c39ef51880475356872d5346360
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-25 21:48:53 -0300

    feat(sales): add validation to AddPurchaseItemCommand

--- src/store/sales/app/AddPurchaseItemCommand.ts
+++ src/store/sales/app/AddPurchaseItemCommand.ts
@@ -1,5 +1,7 @@
 import { EventData } from '@shared/@types/events';
 import Command from '@shared/abstractions/Command';
+import { GenericError } from '@shared/errors/ValidationError';
+import Validator from '@shared/utils/Validator';
 
 export type AddPurchaseItemData = {
   clientId: string;
@@ -10,13 +12,31 @@ export type AddPurchaseItemData = {
 }
 
 export default class AddPurchaseItemCommand extends Command<boolean, AddPurchaseItemData> {
+  private _errors: GenericError[] = [];
+
+  get errors() {
+    return this._errors;
+  }
+
   public send(data: EventData<AddPurchaseItemData>): Promise<boolean | void> {
     console.info(data);
     return Promise.resolve(true);
   }
 
   public validate(data: EventData<AddPurchaseItemData>): boolean {
-    console.info(data);
-    throw new Error('Method not implemented.');
+    const errors = Validator.setData(data)
+      .setRule('clientId', ['required', 'string'])
+      .setRule('productId', ['required', 'string'])
+      .setRule('productName', ['required', 'string'])
+      .setRule('productAmount', ['required', 'number'])
+      .setRule('quantity', ['required', 'number', 'min:1', 'max:10'])
+      .validate();
+
+    if (errors.length) {
+      this._errors = errors;
+      return true;
+    }
+
+    return false;
   }
 }
