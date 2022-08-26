baa6342fb51b95cbc6034d101864e5a7ac7120b3
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-26 20:21:22 -0300

    chore(sales): define IPurchaseOrderRepository interface

--- src/store/sales/domain/IPurchaseOrderRepository.ts
+++ src/store/sales/domain/IPurchaseOrderRepository.ts
@@ -1,3 +1,31 @@
-interface IPurchaseOrderRepository {}
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
+import PurchaseOrder from './PurchaseOrder';
+import PurchaseOrderItem from './PurchaseOrderItem';
+import Voucher from './Voucher';
+
+interface IPurchaseOrderRepository {
+  getPurchaseOrderById(id: string): Promise<PurchaseOrder | null>;
+
+  getPurchaseOrdersByClientId(id: string): Promise<PurchaseOrder[]>;
+
+  getDraftPurchaseOrderByClientId(id: string): Promise<PurchaseOrder | null>;
+
+  addPurchaseOrder(purchaseOrder: PurchaseOrder): Promise<PurchaseOrder>;
+
+  updatePurchaseOrder(purchaseOrder: PurchaseOrder): Promise<PurchaseOrder>;
+
+  getPurchaseOrderItemById(id: string): Promise<PurchaseOrderItem | null>;
+
+  getPurchaseOrderItem(purchaseOrderId: string, productId: string): Promise<PurchaseOrderItem | null>;
+
+  addPurchaseOrderItem(purchaseOrderItem: PurchaseOrderItem): Promise<PurchaseOrderItem>;
+
+  updatePurchaseOrderItem(purchaseOrderItem: PurchaseOrderItem): Promise<PurchaseOrderItem>;
+
+  deletePurchaseOrderItem(purchaseOrderItem: PurchaseOrderItem): Promise<boolean>;
+
+  getVoucherByCode(code: string): Promise<Voucher | null>;
+}
 
 export default IPurchaseOrderRepository;
