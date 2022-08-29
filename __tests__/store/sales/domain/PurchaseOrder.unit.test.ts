bcc426bfdb26dc631b7a3a1346d186a73c1aa3b5
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-29 20:48:07 -0300

    test(sales): create test to ensures that PurchaseOrder.addItem updates item quantity

--- __tests__/store/sales/domain/PurchaseOrder.unit.test.ts
+++ __tests__/store/sales/domain/PurchaseOrder.unit.test.ts
@@ -51,5 +51,37 @@ describe("PurchaseOrder's unit tests", () => {
       expect(purchaseOrder.items).toHaveLength(1);
       expect(purchaseOrder.items[0]).toEqual(purchaseOrderItem);
     });
+
+    it('increments the quantity of an existing purchase order item', () => {
+      const purchaseOrder = new PurchaseOrder({
+        id: faker.datatype.uuid(),
+        clientId: faker.datatype.uuid(),
+        code: parseInt(faker.datatype.number().toString(), 10),
+        createdAt: new Date(),
+      });
+
+      const params = {
+        id: faker.datatype.uuid(),
+        quantity: parseInt(faker.datatype.number().toString(), 10),
+        purchaseOrderId: faker.datatype.uuid(),
+        product: new Product(
+          faker.datatype.uuid(),
+          faker.commerce.product(),
+          faker.datatype.float(),
+        ),
+      };
+
+      const purchaseOrderItem = new PurchaseOrderItem(params);
+      const clonePurchaseOrderItem = new PurchaseOrderItem({
+        ...params,
+        quantity: 1,
+      });
+
+      purchaseOrder.addItem(purchaseOrderItem);
+      purchaseOrder.addItem(clonePurchaseOrderItem);
+
+      expect(purchaseOrder.items).toHaveLength(1);
+      expect(purchaseOrder.items[0].quantity).toEqual(params.quantity + 1);
+    });
   });
 });
