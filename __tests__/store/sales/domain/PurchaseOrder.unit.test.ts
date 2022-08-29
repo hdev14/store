506a82afaf9bfe2eff9e20620b95cd7685eb76a0
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-29 20:42:30 -0300

    test(sales): create test to ensures that PurchaseOrder.addItem works

--- __tests__/store/sales/domain/PurchaseOrder.unit.test.ts
+++ __tests__/store/sales/domain/PurchaseOrder.unit.test.ts
@@ -1,24 +1,55 @@
 import { faker } from '@faker-js/faker';
-import PurchaseOrder, { DraftPurchaseOrderParams, PurchaseOrderStatus } from '@sales/domain/PurchaseOrder';
+import Product from '@sales/domain/Product';
+import PurchaseOrder, { PurchaseOrderParams, PurchaseOrderStatus } from '@sales/domain/PurchaseOrder';
+import PurchaseOrderItem from '@sales/domain/PurchaseOrderItem';
 
 describe("PurchaseOrder's unit tests", () => {
-  it('creates a draft purchase order', () => {
-    const params: DraftPurchaseOrderParams = {
-      id: faker.datatype.uuid(),
-      clientId: faker.datatype.uuid(),
-      code: parseInt(faker.datatype.number().toString(), 10),
-      createdAt: new Date(),
-    };
+  describe('createDraft', () => {
+    it('creates a draft purchase order', () => {
+      const params: PurchaseOrderParams = {
+        id: faker.datatype.uuid(),
+        clientId: faker.datatype.uuid(),
+        code: parseInt(faker.datatype.number().toString(), 10),
+        createdAt: new Date(),
+      };
 
-    const draftPurchaseOrder = PurchaseOrder.createDraft(params);
+      const draftPurchaseOrder = PurchaseOrder.createDraft(params);
 
-    expect(draftPurchaseOrder).toEqual({
-      ...params,
-      status: PurchaseOrderStatus.DRAFT,
-      voucher: undefined,
-      discountAmount: 0,
-      totalAmount: 0,
-      _items: [],
+      expect(draftPurchaseOrder).toEqual({
+        ...params,
+        status: PurchaseOrderStatus.DRAFT,
+        voucher: undefined,
+        discountAmount: 0,
+        totalAmount: 0,
+        _items: [],
+      });
+    });
+  });
+
+  describe('addItem', () => {
+    it('adds a new purchase order item', () => {
+      const purchaseOrder = new PurchaseOrder({
+        id: faker.datatype.uuid(),
+        clientId: faker.datatype.uuid(),
+        code: parseInt(faker.datatype.number().toString(), 10),
+        createdAt: new Date(),
+      });
+
+      const purchaseOrderItem = new PurchaseOrderItem({
+        id: faker.datatype.uuid(),
+        quantity: parseInt(faker.datatype.number().toString(), 10),
+        purchaseOrderId: faker.datatype.uuid(),
+        product: new Product(
+          faker.datatype.uuid(),
+          faker.commerce.product(),
+          faker.datatype.float(),
+        ),
+      });
+
+      purchaseOrder.addItem(purchaseOrderItem);
+
+      expect(purchaseOrder.items).toHaveLength(1);
+      expect(purchaseOrder.items[0]).toEqual(purchaseOrderItem);
     });
   });
 });
