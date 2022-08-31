8b9568fb127a41826fb01b4660daa72be8e7dbe1
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-08-30 21:05:02 -0300

    test(sales): create PrismaPurchaseOrderRepository suite of tests

---
+++ __tests__/store/sales/infra/PrismaPurchaseOrderRepository.unit.test.ts
@@ -0,0 +1,45 @@
+describe("PrismaPurchaseOrderRepository's unit tests", () => {
+  describe('PrismaPurchaseOrderRepository.getPurchaseOrderById()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.getPurchaseOrdersByClientId()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.getDraftPurchaseOrderByClientId()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.addPurchaseOrder()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.updatePurchaseOrder()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.getPurchaseOrderItemById()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.getPurchaseOrderItem()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.addPurchaseOrderItem()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.updatePurchaseOrderItem()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.deletePurchaseOrderItem()', () => {
+
+  });
+
+  describe('PrismaPurchaseOrderRepository.getVoucherByCode()', () => {
+
+  });
+});
