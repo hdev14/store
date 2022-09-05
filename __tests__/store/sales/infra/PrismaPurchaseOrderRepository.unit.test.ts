46b3e9b1105836c1ceb806445353d4264a9f0969
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-05 20:41:12 -0300

    test(sales): create test to ensures that repository.udpatePurchaseOrderItem works

--- __tests__/store/sales/infra/PrismaPurchaseOrderRepository.unit.test.ts
+++ __tests__/store/sales/infra/PrismaPurchaseOrderRepository.unit.test.ts
@@ -780,7 +780,66 @@ describe("PrismaPurchaseOrderRepository's unit tests", () => {
   });
 
   describe('PrismaPurchaseOrderRepository.updatePurchaseOrderItem()', () => {
+    it('updates a specific purchase order item', async () => {
+      expect.assertions(7);
 
+      const fakePurchaseOrderItem = {
+        id: faker.datatype.uuid(),
+        quantity: parseInt(faker.datatype.number().toString(), 10),
+        purchaseOrderId: faker.datatype.uuid(),
+        product: {
+          id: faker.datatype.uuid(),
+          name: faker.commerce.product(),
+          amount: faker.datatype.float(),
+        },
+      };
+
+      prismaMock.purchaseOrderItem.update.mockResolvedValueOnce(fakePurchaseOrderItem as any);
+
+      const repository = new PrismaPurchaseOrderRepository();
+
+      const purchaseOrderItem = await repository.updatePurchaseOrderItem(
+        new PurchaseOrderItem({
+          id: fakePurchaseOrderItem.id,
+          purchaseOrderId: fakePurchaseOrderItem.purchaseOrderId,
+          quantity: fakePurchaseOrderItem.quantity,
+          product: new Product(
+            fakePurchaseOrderItem.product.id,
+            fakePurchaseOrderItem.product.name,
+            fakePurchaseOrderItem.product.amount,
+          ),
+        }),
+      );
+
+      expect(purchaseOrderItem!.id).toEqual(fakePurchaseOrderItem.id);
+      expect(purchaseOrderItem!.quantity).toEqual(fakePurchaseOrderItem.quantity);
+      expect(purchaseOrderItem!.purchaseOrderId).toEqual(fakePurchaseOrderItem.purchaseOrderId);
+      expect(purchaseOrderItem!.purchaseOrderId).toEqual(fakePurchaseOrderItem.purchaseOrderId);
+      expect(purchaseOrderItem!.product).toEqual(new Product(
+        fakePurchaseOrderItem.product.id,
+        fakePurchaseOrderItem.product.name,
+        fakePurchaseOrderItem.product.amount,
+      ));
+
+      expect(prismaMock.purchaseOrderItem.update).toHaveBeenCalledTimes(1);
+      expect(prismaMock.purchaseOrderItem.update).toHaveBeenCalledWith({
+        where: { id: fakePurchaseOrderItem.id },
+        data: {
+          purchaseOrderId: fakePurchaseOrderItem.purchaseOrderId,
+          quantity: fakePurchaseOrderItem.quantity,
+          productId: fakePurchaseOrderItem.product.id,
+        },
+        include: {
+          product: {
+            select: {
+              id: true,
+              name: true,
+              amount: true,
+            },
+          },
+        },
+      });
+    });
   });
 
   describe('PrismaPurchaseOrderRepository.deletePurchaseOrderItem()', () => {
