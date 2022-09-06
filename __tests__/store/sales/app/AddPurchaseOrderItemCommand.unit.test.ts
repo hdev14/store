25deae636c5fb70e7ed1a1d450e769290c9cadb0
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-06 20:33:13 -0300

    test(sales): create test to ensures that AddPurchaseOrderItemCommand.send validates the command data

--- __tests__/store/sales/app/AddPurchaseOrderItemCommand.unit.test.ts
+++ __tests__/store/sales/app/AddPurchaseOrderItemCommand.unit.test.ts
@@ -1,3 +1,34 @@
-describe("AddPurchaseOrderItemCommand's unit tests", () => {
+import AddPurchaseOrderItemCommand from '@sales/app/AddPurchaseOrderItemCommand';
+import { EventData } from '@shared/@types/events';
+import EventMediator from '@shared/abstractions/EventMediator';
+import ValidationError from '@shared/errors/ValidationError';
 
+class MediatorStub extends EventMediator {
+  public send<R, T = {}>(_name: string, _data: EventData<T>): Promise<void | R> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+describe("AddPurchaseOrderItemCommand's unit tests", () => {
+  it('throws an exception of type ValidationError if command data is invalid', async () => {
+    expect.assertions(2);
+    const addPurchaseOrderItemCommand = new AddPurchaseOrderItemCommand(new MediatorStub());
+
+    try {
+      const invalidData: any = {
+        clientId: 'wrong',
+        principalId: 'wrong',
+        productAmount: 'wrong',
+        productId: 'wrong',
+        productName: 123,
+        quantity: 'wrong',
+        timestamp: new Date().toISOString(),
+      };
+
+      await addPurchaseOrderItemCommand.send(invalidData);
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(ValidationError);
+      expect(e.errors).toHaveLength(6);
+    }
+  });
 });
