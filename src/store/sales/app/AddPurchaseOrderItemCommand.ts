62ecc039d910db63c1945e3a88d35ec82f52f1d0
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-06 20:32:21 -0300

    feat(sales): ensure that AddPurchaseOrderItemCommand's data is validated

--- src/store/sales/app/AddPurchaseOrderItemCommand.ts
+++ src/store/sales/app/AddPurchaseOrderItemCommand.ts
@@ -1,6 +1,5 @@
 import { EventData } from '@shared/@types/events';
 import Command from '@shared/abstractions/Command';
-import { GenericError } from '@shared/errors/ValidationError';
 import Validator from '@shared/utils/Validator';
 
 export type AddPurchaseOrderItemData = {
@@ -13,37 +12,22 @@ export type AddPurchaseOrderItemData = {
 
 // eslint-disable-next-line max-len
 export default class AddPurchaseOrderItemCommand extends Command<boolean, AddPurchaseOrderItemData> {
-  private _errors: GenericError[] = [];
-
-  get errors() {
-    return this._errors;
-  }
-
   public async send(data: EventData<AddPurchaseOrderItemData>): Promise<boolean | void> {
-    if (!this.validate(data)) {
-      // TODO: throws an error
-      return false;
-    }
+    this.validate(data);
 
     const result = await this.mediator.send<boolean>(this.constructor.name, data);
 
     return result;
   }
 
-  public validate(data: EventData<AddPurchaseOrderItemData>): boolean {
-    const errors = Validator.setData(data)
-      .setRule('clientId', ['required', 'string'])
-      .setRule('productId', ['required', 'string'])
+  public validate(data: EventData<AddPurchaseOrderItemData>): void {
+    Validator.setData(data)
+      .setRule('principalId', ['required', 'string', 'uuid'])
+      .setRule('clientId', ['required', 'string', 'uuid'])
+      .setRule('productId', ['required', 'string', 'uuid'])
       .setRule('productName', ['required', 'string'])
       .setRule('productAmount', ['required', 'number'])
       .setRule('quantity', ['required', 'number', 'min:1', 'max:10'])
       .validate();
-
-    if (errors.length) {
-      this._errors = errors;
-      return true;
-    }
-
-    return false;
   }
 }
--- src/store/shared/abstractions/Command.ts
+++ src/store/shared/abstractions/Command.ts
@@ -3,5 +3,5 @@ import { EventData } from '@shared/@types/events';
 import Event from './Event';
 
 export default abstract class Command<R, T> extends Event<R, T> {
-  public abstract validate(data: EventData<T>): boolean;
+  public abstract validate(data: EventData<T>): boolean | void;
 }
