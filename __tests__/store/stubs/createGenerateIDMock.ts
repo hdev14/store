c04e3406828e7632d41b11d0ef1b16d8f3155ab8
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-06 21:34:02 -0300

    test: improve createGenerateIDMock function

--- __tests__/store/stubs/createGenerateIDMock.ts
+++ __tests__/store/stubs/createGenerateIDMock.ts
@@ -1,7 +1,8 @@
+import { faker } from '@faker-js/faker';
 import IGenerateID from '@shared/utils/IGenerateID';
 
-function createGenerateIDMock(items: any): IGenerateID {
-  return jest.fn(() => `test_product_id_${items.length + 1}`);
+function createGenerateIDMock(items: any[] = []): IGenerateID {
+  return jest.fn(() => (items.length ? `test_id_${items.length + 1}` : faker.datatype.uuid()));
 }
 
 export default createGenerateIDMock;
