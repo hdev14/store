d6cf73877408d3998ad006708329d7b71eeb6be4
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-06 21:34:58 -0300

    feat(sales): ensure that AddPurchaseOrderItemCommandHandler instanciate a new PurchaseOrderItem

--- src/store/sales/app/AddPurchaseOrderItemCommandHandler.ts
+++ src/store/sales/app/AddPurchaseOrderItemCommandHandler.ts
@@ -3,24 +3,28 @@ import Product from '@sales/domain/Product';
 import PurchaseOrder from '@sales/domain/PurchaseOrder';
 import PurchaseOrderItem from '@sales/domain/PurchaseOrderItem';
 import { EventData, IEventHandler } from '@shared/@types/events';
+import IGenerateID from '@shared/utils/IGenerateID';
 import { AddPurchaseOrderItemData } from './AddPurchaseOrderItemCommand';
 
 export default class AddPurchaseItemCommandHandler implements IEventHandler<boolean> {
   private repository: IPurchaseOrderRepository;
 
-  constructor(repository: IPurchaseOrderRepository) {
+  private generateID: IGenerateID;
+
+  constructor(repository: IPurchaseOrderRepository, generateID: IGenerateID) {
     this.repository = repository;
+    this.generateID = generateID;
   }
 
   public async handle(data: EventData<AddPurchaseOrderItemData>): Promise<void | boolean> {
-    // TODO: improve this logic
-
-    console.info(data);
-
+    // TODO:
+    // Add try/catch to return FALSE if something goes wrong
+    // Create a function to perform each individual logic
     const draftPurchaseOrder = await this.repository.getDraftPurchaseOrderByClientId(data.clientId);
 
     const purchaseOrderItem = new PurchaseOrderItem({
-      id: 'asdfa',
+      id: this.generateID(),
+      purchaseOrderId: data.principalId,
       product: new Product(data.productId, data.productName, data.productAmount),
       quantity: data.quantity,
     });
