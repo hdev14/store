ea0b190bd8bc8ad22cf0252c92245d7c8c7291a2
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-09 20:45:54 -0300

    feat(shared): create EventPublisher class

---
+++ src/store/shared/EventPublisher.ts
@@ -0,0 +1,28 @@
+import { EventData } from './@types/events';
+import { EventConstructor } from './abstractions/Event';
+import EventMediator from './abstractions/EventMediator';
+
+export default class EventPublisher {
+  private mediator: EventMediator;
+
+  private events: Array<{ ctor: EventConstructor, data: EventData }>;
+
+  constructor(mediator: EventMediator) {
+    this.mediator = mediator;
+    this.events = [];
+  }
+
+  addEvent(ctor: EventConstructor, data: EventData) {
+    this.events.push({ ctor, data });
+  }
+
+  async sendEvents(): Promise<void> {
+    const promises = this.events.map(async (event) => {
+      const UnknownEvent = event.ctor;
+      const unknownEvent = new UnknownEvent(this.mediator);
+      await unknownEvent.send(event.data);
+    });
+
+    await Promise.all(promises);
+  }
+}
--- src/store/shared/abstractions/Event.ts
+++ src/store/shared/abstractions/Event.ts
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import { EventData } from '@shared/@types/events';
 import EventMediator from './EventMediator';
 
-export default abstract class Event<R = {}, T = {}> {
+export abstract class Event<R = {}, T = {}> {
   protected mediator: EventMediator;
 
   constructor(mediator: EventMediator) {
@@ -12,3 +13,9 @@ export default abstract class Event<R = {}, T = {}> {
     return this.mediator.send<R>(this.constructor.name, data);
   }
 }
+
+export interface EventConstructor {
+  new(mediator: EventMediator): Event;
+}
+
+export default Event;
