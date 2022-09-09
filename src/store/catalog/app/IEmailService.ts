2f14130aefe13ae18a5ab6a29cf8655488a5ac34
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-09 15:00:36 -0300

    refactor(catalog): simplify email service interface

--- src/store/catalog/app/IEmailService.ts
+++ src/store/catalog/app/IEmailService.ts
@@ -1,28 +1,15 @@
 /* eslint-disable no-unused-vars */
 type Email = string;
 
-type EmailDefaultParams = {
+type EmailParams = {
   from: Email;
   subject: string;
   to: Email | Email[];
-}
-
-type EmailTextParams = EmailDefaultParams & {
-  text: string;
-}
-
-type EmailHtmlParams = EmailDefaultParams & {
-  html: string;
-}
-
-type EmailParams = EmailDefaultParams & {
   text?: string;
   html?: string;
 }
 
 interface IEmailService {
-  sendText(params: EmailTextParams): Promise<void>;
-  sendHtml(params: EmailHtmlParams): Promise<void>;
   send(params: EmailParams): Promise<void>;
 }
 
