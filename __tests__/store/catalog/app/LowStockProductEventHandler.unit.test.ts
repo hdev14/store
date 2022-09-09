b1dbf025178dd4b676a546e73f8fa28dfa20bb62
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-09 16:37:21 -0300

    test(catalog): create test to ensures that LowStockProductEventHandler calls EmailService.send method

---
+++ __tests__/store/catalog/app/LowStockProductEventHandler.unit.test.ts
@@ -0,0 +1,40 @@
+import IEmailService, { EmailParams } from '@catalog/app/IEmailService';
+import LowStockProductEventHandler, { } from '@catalog/app/LowStockProductEventHandler';
+import { LowStockProductData } from '@catalog/domain/LowStockProductEvent';
+import { faker } from '@faker-js/faker';
+import { EventData } from '@shared/@types/events';
+
+class EmailServiceStub implements IEmailService {
+  send(_: EmailParams): Promise<void> {
+    return Promise.resolve();
+  }
+}
+
+describe("LowStockProductEventHandler's unit tests", () => {
+  it('calls EmailService.send with correct params', async () => {
+    expect.assertions(2);
+
+    const emailServiceStub = new EmailServiceStub();
+    const sendSpy = jest.spyOn(emailServiceStub, 'send');
+
+    const lowStockProductEventHandler = new LowStockProductEventHandler(emailServiceStub);
+
+    const data: EventData<LowStockProductData> = {
+      principalId: faker.datatype.uuid(),
+      productName: faker.commerce.product(),
+      productQuantity: parseInt(faker.datatype.number().toString(), 10),
+      timestamp: new Date().toISOString(),
+    };
+
+    await lowStockProductEventHandler.handle(data);
+
+    expect(sendSpy).toHaveBeenCalledTimes(1);
+    expect(sendSpy).toHaveBeenCalledWith({
+      from: 'default@email.com',
+      to: 'default@email.com',
+      subject: 'Produto com baixo estoque!',
+      text: `O produto ${data.productName} com ID ${data.principalId}, está apenas com ${data.productQuantity} de quantitade.`,
+      html: `<p>O produto ${data.productName} com ID ${data.principalId}, está apenas com ${data.productQuantity} de quantitade.</p>`,
+    });
+  });
+});
