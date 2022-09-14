b41b33d1c064fbaf0bc561283904e4911ae88b17
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-14 19:19:42 -0300

    test(shared): create test to ensures that EvenPublisher.sendEvents calls all the event send methods

--- __tests__/store/shared/EventPublisher.unit.test.ts
+++ __tests__/store/shared/EventPublisher.unit.test.ts
@@ -34,4 +34,31 @@ describe("EventPublisher's unit tests", () => {
     expect(fakeEvent2).toHaveBeenCalledTimes(1);
     expect(fakeEvent3).toHaveBeenCalledTimes(1);
   });
+
+  it('calls all event send methods', async () => {
+    expect.assertions(4);
+
+    const mediatorSub = new MediatorSub();
+
+    const publisher = new EventPublisher(mediatorSub);
+
+    const fakeEventObject = {
+      send: jest.fn().mockImplementation(() => console.info('test')),
+    };
+
+    const fakeEvent1 = jest.fn().mockImplementation(() => fakeEventObject);
+    const fakeEvent2 = jest.fn().mockImplementation(() => fakeEventObject);
+    const fakeEvent3 = jest.fn().mockImplementation(() => fakeEventObject);
+
+    publisher.addEvent(fakeEvent1, { principalId: 'test1', timestamp: new Date().toISOString() });
+    publisher.addEvent(fakeEvent2, { principalId: 'test2', timestamp: new Date().toISOString() });
+    publisher.addEvent(fakeEvent3, { principalId: 'test3', timestamp: new Date().toISOString() });
+
+    await publisher.sendEvents();
+
+    expect(fakeEventObject.send).toHaveBeenCalledTimes(3);
+    expect(fakeEventObject.send.mock.calls[0][0].principalId).toEqual('test1');
+    expect(fakeEventObject.send.mock.calls[1][0].principalId).toEqual('test2');
+    expect(fakeEventObject.send.mock.calls[2][0].principalId).toEqual('test3');
+  });
 });
