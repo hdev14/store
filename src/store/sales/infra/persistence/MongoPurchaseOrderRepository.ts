daed0cb67233b8b396a841ce740d3182e9ef46d1
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-16 21:04:25 -0300

    feat(sales): ensure that MongoPurchaseOrderRepository.getPurchaseOrderById works

--- src/store/sales/infra/persistence/MongoPurchaseOrderRepository.ts
+++ src/store/sales/infra/persistence/MongoPurchaseOrderRepository.ts
@@ -1,7 +1,11 @@
 import IPurchaseOrderRepository from '@sales/domain/IPurchaseOrderRepository';
-import PurchaseOrder from '@sales/domain/PurchaseOrder';
+import Product from '@sales/domain/Product';
+import PurchaseOrder, { PurchaseOrderParams, PurchaseOrderStatus } from '@sales/domain/PurchaseOrder';
 import PurchaseOrderItem from '@sales/domain/PurchaseOrderItem';
 import Voucher from '@sales/domain/Voucher';
+import { IPurchaseOrderItem } from '@mongoose/PurchaseOrderItemModel';
+import PurchaseOrderModel, { IPurchaseOrder } from '@mongoose/PurchaseOrderModel';
+import { IVoucher } from '@mongoose/VoucherModel';
 
 export default class MongoPurchaseOrderRepository implements IPurchaseOrderRepository {
   countPurchaseOrders(): Promise<number> {
@@ -16,8 +20,20 @@ export default class MongoPurchaseOrderRepository implements IPurchaseOrderRepos
     throw new Error('Method not implemented.');
   }
 
-  getPurchaseOrderById(id: string): Promise<PurchaseOrder | null> {
-    throw new Error('Method not implemented.');
+  public async getPurchaseOrderById(id: string): Promise<PurchaseOrder | null> {
+    const purchaseOrder = await PurchaseOrderModel
+      .findOne({ id })
+      .populate('client')
+      .populate('voucher')
+      .populate({
+        path: 'items',
+        populate: {
+          path: 'product',
+          select: 'id name amount',
+        },
+      });
+
+    return purchaseOrder ? this.mapPurchaseOrder(purchaseOrder) : null;
   }
 
   getPurchaseOrdersByClientId(id: string): Promise<PurchaseOrder[]> {
@@ -64,5 +80,55 @@ export default class MongoPurchaseOrderRepository implements IPurchaseOrderRepos
 
   deletePurchaseOrderItem(purchasOrderItemId: string): Promise<boolean> {
     throw new Error('Method not implemented.');
+  }
+
+  private mapPurchaseOrder(purchaseOrder: IPurchaseOrder): PurchaseOrder {
+    const params: PurchaseOrderParams = {
+      id: purchaseOrder.id,
+      clientId: purchaseOrder.client.id,
+      code: purchaseOrder.code,
+      totalAmount: purchaseOrder.totalAmount,
+      discountAmount: purchaseOrder.discountAmount,
+      status: purchaseOrder.status as PurchaseOrderStatus,
+      createdAt: purchaseOrder.createdAt,
+      voucher: purchaseOrder.voucher ? this.mapVoucher(purchaseOrder.voucher) : null,
+    };
+
+    const po = new PurchaseOrder(params);
+
+    if (purchaseOrder.items) {
+      purchaseOrder.items.forEach((item) => {
+        po.addItem(this.mapPurchaseOrderItem(item));
+      });
+    }
+
+    return po;
+  }
+
+  private mapVoucher(voucher: IVoucher) {
+    return new Voucher({
+      id: voucher.id,
+      active: voucher.active,
+      code: voucher.code,
+      percentageAmount: voucher.percentageAmount,
+      rawDiscountAmount: voucher.rawDiscountAmount,
+      type: voucher.type,
+      quantity: voucher.quantity,
+      createdAt: voucher.createdAt,
+      expiresAt: voucher.expiresAt,
+      usedAt: voucher.usedAt,
+    });
+  }
+
+  private mapPurchaseOrderItem(purchaseOrderItem: IPurchaseOrderItem) {
+    return new PurchaseOrderItem({
+      id: purchaseOrderItem.id,
+      quantity: purchaseOrderItem.quantity,
+      product: new Product(
+        purchaseOrderItem.product.id,
+        purchaseOrderItem.product.name,
+        purchaseOrderItem.product.amount,
+      ),
+    });
   }
 }
