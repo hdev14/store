01e34567edc0e2e3a0ab0540eb160da260a3066a
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-19 21:00:49 -0300

    refactor(shared): improve RepositoryError constructor

--- src/store/shared/errors/RepositoryError.ts
+++ src/store/shared/errors/RepositoryError.ts
@@ -1,8 +1,5 @@
 export default class RepositoryError extends Error {
-  public repository: string;
-
   constructor(repository: string, message: string) {
-    super(message);
-    this.repository = repository;
+    super(`${repository} - ${message}`);
   }
 }
