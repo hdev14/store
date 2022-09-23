1807e3b94343a3f08280c2668ce22ca3b2a9ac58
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-23 20:42:50 -0300

    test(sales): create test to ensures that the handler throws an EventHandllerError

--- __tests__/store/sales/app/UpdatePurchaseOrderEventHandler.unit.test.ts
+++ __tests__/store/sales/app/UpdatePurchaseOrderEventHandler.unit.test.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker';
 import { UpdateDraftPurchaseOrderEventData } from '@sales/app/events/UpdateDraftPurchaseOrderEvent';
 import UpdateDraftPurchaseOrderEventHandler from '@sales/app/events/UpdateDraftPurchaseOrderEventHandler';
 import { EventData } from '@shared/@types/events';
+import EventHandlerError from '@shared/errors/EventHandlerError';
 import RepositoryStub from '../../stubs/PurchaseOrderRepositoryStub';
 
 describe("UpdateDraftPurchaseOrderEventHandler's unit tests", () => {
@@ -33,5 +34,32 @@ describe("UpdateDraftPurchaseOrderEventHandler's unit tests", () => {
     expect(updatePurchaseOrderSpy.mock.calls[0][0].totalAmount).toEqual(eventData.totalAmount);
     expect(updatePurchaseOrderSpy.mock.calls[0][0].discountAmount)
       .toEqual(eventData.discountAmount);
+  });
+
+  it('throws an EventHandlerError when occurs an expected error', async () => {
+    expect.assertions(2);
+
+    const repository = new RepositoryStub();
+
+    repository.updatePurchaseOrder = jest.fn().mockRejectedValueOnce(new Error('test'));
+
+    const handler = new UpdateDraftPurchaseOrderEventHandler(repository);
+
+    const eventData: EventData<UpdateDraftPurchaseOrderEventData> = {
+      eventType: 'UpdateDraftPurchaseOrderEvent',
+      principalId: faker.datatype.uuid(),
+      customerId: faker.datatype.uuid(),
+      code: parseInt(faker.datatype.number().toString(), 10),
+      discountAmount: faker.datatype.float(),
+      totalAmount: faker.datatype.float(),
+      timestamp: new Date().toISOString(),
+    };
+
+    try {
+      await handler.handle(eventData);
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(EventHandlerError);
+      expect(e.message).toEqual('Erro ao atualizar o pedido.');
+    }
   });
 });
