1e0984fe4d93ae189366130f8c8a386d5f2f597e
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-23 19:24:37 -0300

    feat(sales): ensure that AddDraftPurchaseOrderEventHandler calls repository.addPurchaseOrder when receive an event data

--- src/store/sales/app/events/AddDraftPurchaseOrderEventHandler.ts
+++ src/store/sales/app/events/AddDraftPurchaseOrderEventHandler.ts
@@ -1,9 +1,25 @@
+import { IPurchaseOrderRepositoryCommands } from '@sales/domain/IPurchaseOrderRepository';
+import PurchaseOrder from '@sales/domain/PurchaseOrder';
 import { EventData, IEventHandler } from '@shared/@types/events';
 import { AddDraftPurchaseOrderEventData } from './AddDraftPurchaseOrderEvent';
 
 export default class AddDraftPurchaseOrderEventHandler implements IEventHandler<void> {
-  handle(data: EventData<AddDraftPurchaseOrderEventData>): Promise<void> {
-    console.info(data);
-    throw new Error('Method not implemented.');
+  private repository: IPurchaseOrderRepositoryCommands;
+
+  constructor(repository: IPurchaseOrderRepositoryCommands) {
+    this.repository = repository;
+  }
+
+  public async handle(data: EventData<AddDraftPurchaseOrderEventData>): Promise<void> {
+    const draftPurchaseOrder = PurchaseOrder.createDraft({
+      id: data.principalId,
+      code: data.code,
+      customerId: data.customerId,
+      createdAt: data.createdAt,
+      status: null,
+      voucher: null,
+    });
+
+    await this.repository.addPurchaseOrder(draftPurchaseOrder);
   }
 }
