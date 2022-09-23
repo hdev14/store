4d8079a72719465460b8df1f56076bf7ec1504a6
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-23 20:02:13 -0300

    feat(sales): ensure that AddPurchaseOrderItemEventHandler throws an EventHandlerError

--- src/store/sales/app/events/AddPurchaseOrderItemEventHandler.ts
+++ src/store/sales/app/events/AddPurchaseOrderItemEventHandler.ts
@@ -2,6 +2,7 @@ import { IPurchaseOrderRepositoryCommands } from '@sales/domain/IPurchaseOrderRe
 import Product from '@sales/domain/Product';
 import PurchaseOrderItem from '@sales/domain/PurchaseOrderItem';
 import { EventData, IEventHandler } from '@shared/@types/events';
+import EventHandlerError from '@shared/errors/EventHandlerError';
 import { AddPurchaseOrderItemEventData } from './AddPurchaseOrderItemEvent';
 
 export default class AddPurchaseOrderItemEventHandler implements IEventHandler<void> {
@@ -12,17 +13,22 @@ export default class AddPurchaseOrderItemEventHandler implements IEventHandler<v
   }
 
   public async handle(data: EventData<AddPurchaseOrderItemEventData>): Promise<void> {
-    const purchaseOrderItem = new PurchaseOrderItem({
-      id: data.principalId,
-      quantity: data.quantity,
-      purchaseOrderId: data.purchaseOrderId,
-      product: new Product(
-        data.productId,
-        data.productName,
-        data.productAmount,
-      ),
-    });
+    try {
+      const purchaseOrderItem = new PurchaseOrderItem({
+        id: data.principalId,
+        quantity: data.quantity,
+        purchaseOrderId: data.purchaseOrderId,
+        product: new Product(
+          data.productId,
+          data.productName,
+          data.productAmount,
+        ),
+      });
 
-    await this.repository.addPurchaseOrderItem(purchaseOrderItem);
+      await this.repository.addPurchaseOrderItem(purchaseOrderItem);
+    } catch (e: any) {
+      console.error(e.stack);
+      throw new EventHandlerError('Erro ao cadastrar o item.');
+    }
   }
 }
