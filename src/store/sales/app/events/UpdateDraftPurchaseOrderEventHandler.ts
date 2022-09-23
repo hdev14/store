b0e5f705a9efd8313885db9b6de96fbd65b9029f
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-23 20:42:26 -0300

    feat(sales): ensure that UpdateDraftPurchaseOrderEventHandler throws an EventHandlerError

--- src/store/sales/app/events/UpdateDraftPurchaseOrderEventHandler.ts
+++ src/store/sales/app/events/UpdateDraftPurchaseOrderEventHandler.ts
@@ -1,6 +1,7 @@
 import { IPurchaseOrderRepositoryCommands } from '@sales/domain/IPurchaseOrderRepository';
 import PurchaseOrder from '@sales/domain/PurchaseOrder';
 import { EventData, IEventHandler } from '@shared/@types/events';
+import EventHandlerError from '@shared/errors/EventHandlerError';
 import { UpdateDraftPurchaseOrderEventData } from './UpdateDraftPurchaseOrderEvent';
 
 export default class UpdateDraftPurchaseOrderEventHandler implements IEventHandler<void> {
@@ -11,17 +12,22 @@ export default class UpdateDraftPurchaseOrderEventHandler implements IEventHandl
   }
 
   public async handle(data: EventData<UpdateDraftPurchaseOrderEventData>): Promise<void> {
-    const purchaseOrder = PurchaseOrder.createDraft({
-      id: data.principalId,
-      code: data.code,
-      discountAmount: data.discountAmount,
-      totalAmount: data.totalAmount,
-      customerId: data.customerId,
-      createdAt: new Date(),
-      status: null,
-      voucher: null,
-    });
+    try {
+      const purchaseOrder = PurchaseOrder.createDraft({
+        id: data.principalId,
+        code: data.code,
+        discountAmount: data.discountAmount,
+        totalAmount: data.totalAmount,
+        customerId: data.customerId,
+        createdAt: new Date(),
+        status: null,
+        voucher: null,
+      });
 
-    await this.repository.updatePurchaseOrder(purchaseOrder);
+      await this.repository.updatePurchaseOrder(purchaseOrder);
+    } catch (e: any) {
+      console.error(e.stack);
+      throw new EventHandlerError('Erro ao atualizar o pedido.');
+    }
   }
 }
