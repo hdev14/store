64ec3caddf0bba9dc6edb6fefa9f716baa59f724
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-26 21:22:13 -0300

    test(sales): create tests to ensures that command calls mediator.send method

--- __tests__/store/sales/app/UpdatePurchaseOrderItemQuantityCommand.unit.test.ts
+++ __tests__/store/sales/app/UpdatePurchaseOrderItemQuantityCommand.unit.test.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import UpdatePurchaseOrderItemQuantityCommand, { UpdatePurchaseOrderItemQuantityCommandData } from '@sales/app/commands/UpdatePurchaseOrderItemQuantityCommand';
 import { EventData } from '@shared/@types/events';
 import ValidationError from '@shared/errors/ValidationError';
@@ -21,5 +22,65 @@ describe("UpdatePurchaseOrderItemQuantityCommand's unit tests", () => {
       expect(e).toBeInstanceOf(ValidationError);
       expect(e.errors).toHaveLength(2);
     }
+  });
+
+  it('calls Mediator.send method with correct params', async () => {
+    expect.assertions(2);
+
+    const mediator = new MediatorStub();
+    const sendSpy = jest.spyOn(mediator, 'send');
+
+    const command = new UpdatePurchaseOrderItemQuantityCommand(mediator);
+
+    const data: EventData<UpdatePurchaseOrderItemQuantityCommandData> = {
+      principalId: faker.datatype.uuid(),
+      quantity: parseInt(faker.random.numeric(1), 10), // float
+      timestamp: new Date().toISOString(),
+    };
+
+    await command.send(data);
+
+    expect(sendSpy).toHaveBeenCalledTimes(1);
+    expect(sendSpy).toHaveBeenCalledWith('UpdatePurchaseOrderItemQuantityCommand', data);
+  });
+
+  it('returns TRUE if mediator.send execute successfully', async () => {
+    expect.assertions(1);
+
+    const mediator = new MediatorStub();
+
+    mediator.send = jest.fn().mockResolvedValueOnce(true);
+
+    const command = new UpdatePurchaseOrderItemQuantityCommand(mediator);
+
+    const data: EventData<UpdatePurchaseOrderItemQuantityCommandData> = {
+      principalId: faker.datatype.uuid(),
+      quantity: parseInt(faker.random.numeric(1), 10), // float
+      timestamp: new Date().toISOString(),
+    };
+
+    const result = await command.send(data);
+
+    expect(result).toBe(true);
+  });
+
+  it('returns FALSE if mediator.send fails', async () => {
+    expect.assertions(1);
+
+    const mediator = new MediatorStub();
+
+    mediator.send = jest.fn().mockResolvedValueOnce(false);
+
+    const command = new UpdatePurchaseOrderItemQuantityCommand(mediator);
+
+    const data: EventData<UpdatePurchaseOrderItemQuantityCommandData> = {
+      principalId: faker.datatype.uuid(),
+      quantity: parseInt(faker.random.numeric(1), 10), // float
+      timestamp: new Date().toISOString(),
+    };
+
+    const result = await command.send(data);
+
+    expect(result).toBe(false);
   });
 });
