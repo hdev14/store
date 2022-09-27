799f0e64d8e308f63a36bf576decd29aba7f7a71
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-27 20:39:36 -0300

    feat(sales): ensure that the handler calls publisher methods after the operation

--- src/store/sales/app/commands/UpdatePurchaseOrderItemQuantityCommandHandler.ts
+++ src/store/sales/app/commands/UpdatePurchaseOrderItemQuantityCommandHandler.ts
@@ -1,13 +1,18 @@
 import IPurchaseOrderRepository from '@sales/domain/IPurchaseOrderRepository';
 import { EventData, IEventHandler } from '@shared/@types/events';
+import EventPublisher from '@shared/EventPublisher';
+import UpdatePurchaseOrderItemEvent, { UpdatePurchaserOrderItemEventData } from '../events/UpdatePurchaseOrderItemEvent';
 import { UpdatePurchaseOrderItemQuantityCommandData } from './UpdatePurchaseOrderItemQuantityCommand';
 
 // eslint-disable-next-line max-len
 export default class UpdatePurchaseOrderItemQuantityCommandHandler implements IEventHandler<boolean> {
   private repository: IPurchaseOrderRepository;
 
-  constructor(repository: IPurchaseOrderRepository) {
+  private publisher: EventPublisher;
+
+  constructor(repository: IPurchaseOrderRepository, publisher: EventPublisher) {
     this.repository = repository;
+    this.publisher = publisher;
   }
 
   public async handle(
@@ -24,10 +29,21 @@ export default class UpdatePurchaseOrderItemQuantityCommandHandler implements IE
 
       await this.repository.updatePurchaseOrderItem(purchaseOrderItem);
 
+      this.publisher.addEvent<UpdatePurchaserOrderItemEventData>(UpdatePurchaseOrderItemEvent, {
+        principalId: purchaseOrderItem.id,
+        quantity: purchaseOrderItem.quantity,
+        productId: purchaseOrderItem.product.id,
+        productName: purchaseOrderItem.product.name,
+        productAmount: purchaseOrderItem.product.amount,
+        timestamp: new Date().toISOString(),
+      });
+
       return true;
     } catch (e: any) {
       console.error(e.stack);
       return false;
+    } finally {
+      await this.publisher.sendEvents();
     }
   }
 }
