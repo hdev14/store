e66d4f3210b39e4ae2b24dfa6a531b54b256efc2
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-27 20:09:20 -0300

    test(sales): create test to ensures that the handler calls repository.updatePurchaseOrderItem

--- __tests__/store/sales/app/UpdatePurchaseOrderItemQuantityCommandHandler.unit.test.ts
+++ __tests__/store/sales/app/UpdatePurchaseOrderItemQuantityCommandHandler.unit.test.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker';
 import { UpdatePurchaseOrderItemQuantityCommandData } from '@sales/app/commands/UpdatePurchaseOrderItemQuantityCommand';
 import UpdatePurchaseOrderItemQuantityCommandHandler from '@sales/app/commands/UpdatePurchaseOrderItemQuantityCommandHandler';
+import Product from '@sales/domain/Product';
+import PurchaseOrderItem from '@sales/domain/PurchaseOrderItem';
 import { EventData } from '@shared/@types/events';
 import RepositoryStub from '../../stubs/PurchaseOrderRepositoryStub';
 
@@ -42,5 +44,40 @@ describe("UpdatePurchaseOrderItemQuantityCommandHandler's unit test", () => {
     const result = await handler.handle(data);
 
     expect(result).toBe(false);
+  });
+
+  it('calls repository.updatePurchaseOrderItem if found the item', async () => {
+    expect.assertions(2);
+
+    const repository = new RepositoryStub();
+    const fakePurchaseOrderItem = new PurchaseOrderItem({
+      id: faker.datatype.uuid(),
+      quantity: parseInt(faker.datatype.number().toString(), 10),
+      purchaseOrderId: faker.datatype.uuid(),
+      product: new Product(
+        faker.datatype.uuid(),
+        faker.commerce.product(),
+        faker.datatype.float(),
+      ),
+    });
+
+    repository.getPurchaseOrderItemById = jest.fn().mockResolvedValueOnce(fakePurchaseOrderItem);
+    const updatePurchaseOrderItemSpy = jest.spyOn(repository, 'updatePurchaseOrderItem');
+
+    const handler = new UpdatePurchaseOrderItemQuantityCommandHandler(repository);
+
+    const data: EventData<UpdatePurchaseOrderItemQuantityCommandData> = {
+      principalId: faker.datatype.uuid(),
+      quantity: parseInt(faker.datatype.number().toString(), 10),
+      timestamp: new Date().toISOString(),
+    };
+
+    const expectedPurchaseOrderItem = fakePurchaseOrderItem;
+    expectedPurchaseOrderItem.quantity = data.quantity;
+
+    await handler.handle(data);
+
+    expect(updatePurchaseOrderItemSpy).toHaveBeenCalledTimes(1);
+    expect(updatePurchaseOrderItemSpy).toHaveBeenCalledWith(expectedPurchaseOrderItem);
   });
 });
