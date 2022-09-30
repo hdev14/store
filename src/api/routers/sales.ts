159f824cf58b5f0bd332e367d83360fd913913f9
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-09-30 20:21:20 -0300

    chore(api): add more endpoint to SalesController

--- src/api/routers/sales.ts
+++ src/api/routers/sales.ts
@@ -5,6 +5,16 @@ const router = Router();
 
 const salesController = createSalesController();
 
+router.get('/orders/:id', salesController.getPurchaseOrder.bind(salesController));
+
+router.get('/orders/items/:id', salesController.getPurchaseOrderItem.bind(salesController));
+
 router.post('/orders/items', salesController.addPurchaseOrderItem.bind(salesController));
+
+router.delete('/orders/items/:id', salesController.removePurchaseOrderItem.bind(salesController));
+
+router.patch('/orders/items/:id/quantity', salesController.updatePurchaseOrderItemQuantity.bind(salesController));
+
+router.post('/orders/:id/voucher', salesController.applyVoucher.bind(salesController));
 
 export default router;
