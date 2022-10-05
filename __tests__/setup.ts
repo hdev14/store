52762966f65c9276b5ccda509363d73c67a6e97a
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-10-04 21:00:28 -0300

    chore(test): improve setup logic

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,12 +1,15 @@
 import supertest from 'supertest';
 import Server from '@api/Server';
 import Prisma from '@shared/Prisma';
+import Mongo from '@mongo/index';
 
-beforeAll(() => {
+beforeAll(async () => {
   globalThis.request = supertest(Server.application);
   globalThis.dbConnection = Prisma.connect();
+  await Mongo.connect(process.env.MONGO_URI!);
 });
 
 afterAll(async () => {
   await globalThis.dbConnection.$disconnect();
+  await Mongo.disconnect();
 });
