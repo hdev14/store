c58fb82d485cf4daac72031df819a7564744f641
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-12-09 11:26:54 -0300

    feat(shared): create Query abstract class

--- src/store/shared/abstractions/Query.ts
+++ src/store/shared/abstractions/Query.ts
@@ -1,3 +1,19 @@
-import Event from './Event';
+import Mediator from './Mediator';
 
-export default abstract class Query<T> extends Event<T> { }
+export type Results<D = Record<string, any>> = {
+  results: D[];
+}
+
+export default abstract class Query<D, P = {}> {
+  protected readonly mediator: Mediator;
+
+  constructor(mediator: Mediator) {
+    this.mediator = mediator;
+  }
+
+  public async get(params: P): Promise<Results<D>> {
+    const result = await this.mediator.send<Results<D>>(this.constructor.name, params);
+
+    return result || { results: [] };
+  }
+}
