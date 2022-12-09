156417a979348cc661453eca3958d0ad314b74a6
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-12-09 11:15:03 -0300

    feat(sales): create RemovePurchaseOrderItemEventHandler with the logic to delete an item

---
+++ src/store/sales/app/events/RemovePurchaseOrderItemEventHandler.ts
@@ -0,0 +1,21 @@
+import { IPurchaseOrderRepositoryCommands } from '@sales/domain/IPurchaseOrderRepository';
+import IEventHandler, { EventData } from '@shared/abstractions/IEventHandler';
+import EventHandlerError from '@shared/errors/EventHandlerError';
+
+// eslint-disable-next-line max-len
+export default class RemovePurchaseOrderItemEventHandler implements IEventHandler {
+  private readonly repository: IPurchaseOrderRepositoryCommands;
+
+  constructor(repository: IPurchaseOrderRepositoryCommands) {
+    this.repository = repository;
+  }
+
+  public async handle(data: EventData): Promise<void> {
+    try {
+      await this.repository.deletePurchaseOrderItem(data.principalId);
+    } catch (e: any) {
+      console.error(e.stack);
+      throw new EventHandlerError('Erro ao excluir um item.');
+    }
+  }
+}
