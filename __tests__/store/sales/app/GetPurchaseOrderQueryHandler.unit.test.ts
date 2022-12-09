0fc25321e3114276801415b212ed69988b00008c
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-12-09 15:28:04 -0300

    test(sales): create test to ensures that GetPurchaseOrderQueryHandler returns a purchase order

--- __tests__/store/sales/app/GetPurchaseOrderQueryHandler.unit.test.ts
+++ __tests__/store/sales/app/GetPurchaseOrderQueryHandler.unit.test.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { GetPurchaseOrderParams } from '@sales/app/queries/GetPurchaseOrderQuery';
 import GetPurchaseOrderQueryHandler from '@sales/app/queries/GetPurchaseOrderQueryHandler';
+import PurchaseOrder from '@sales/domain/PurchaseOrder';
 import repositoryStub from '../../stubs/PurchaseOrderRepositoryStub';
 
 describe("GetPurchaseOrderQueryHandler's unit tests", () => {
@@ -18,5 +19,29 @@ describe("GetPurchaseOrderQueryHandler's unit tests", () => {
     await handler.handle(params);
 
     expect(getPurchaseOrderByIdSpy).toHaveBeenCalledWith(params.purchaseOrderId);
+  });
+
+  it('returns a result if purchase order exists', async () => {
+    expect.assertions(1);
+
+    jest.spyOn(repositoryStub, 'getPurchaseOrderById')
+      .mockResolvedValueOnce(new PurchaseOrder({
+        id: faker.datatype.uuid(),
+        customerId: faker.datatype.uuid(),
+        code: parseInt(faker.datatype.number().toString(), 10),
+        createdAt: new Date(),
+        voucher: null,
+        status: null,
+      }));
+
+    const handler = new GetPurchaseOrderQueryHandler(repositoryStub);
+
+    const params: GetPurchaseOrderParams = {
+      purchaseOrderId: faker.datatype.uuid(),
+    };
+
+    const result = await handler.handle(params);
+
+    expect(result.results).toHaveLength(1);
   });
 });
