545a48c3820d9a5d41b2276e9a94306c8ad7c4a2
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-12-11 11:03:35 -0300

    refactor(shared): improve command abstract class

--- src/store/shared/abstractions/Command.ts
+++ src/store/shared/abstractions/Command.ts
@@ -1,3 +1,12 @@
+/* eslint-disable no-unused-vars */
 import Operation from './Operation';
 
-export default abstract class Command<Result, Data> extends Operation<Result | void, Data> {}
+export default abstract class Command<Result, Data> extends Operation<Result | void, Data> {
+  public async execute(data: Data): Promise<Result | void> {
+    this.validate(data);
+
+    return this.mediator.send<Result>(this.constructor.name, data);
+  }
+
+  public abstract validate(data: Data): void;
+}
