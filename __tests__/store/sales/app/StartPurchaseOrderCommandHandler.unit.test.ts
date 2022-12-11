2a92b3a5aa4a3dd48b10ad4103c008ed2df11c0d
Hermerson de Araújo <ne.hermerson@gmail.com>
2022-12-11 13:41:05 -0300

    test(sales): create test to ensures that StartPurchaseOrderCommandHandler returns FALSE

--- __tests__/store/sales/app/StartPurchaseOrderCommandHandler.unit.test.ts
+++ __tests__/store/sales/app/StartPurchaseOrderCommandHandler.unit.test.ts
@@ -98,29 +98,21 @@ describe("StartPurchaseOrderCommandHandler's unit tests", () => {
     expect(updatePurchaseOrderSpy.mock.calls[0][0].status).toBe(PurchaseOrderStatus.STARTED);
   });
 
-  // it('throws an EventHandlerError when occurs an expected error', async () => {
-  //   expect.assertions(2);
+  it('returns FALSE when occurs an unexpected error', async () => {
+    expect.assertions(1);
 
-  //   repositoryStub.addPurchaseOrder = jest.fn().mockRejectedValueOnce(new Error('test'));
+    jest.spyOn(repositoryStub, 'getPurchaseOrderById').mockRejectedValueOnce(new Error('test'));
 
-  //   const handler = new StartPurchaseOrderCommandHandler(repositoryStub);
+    const handler = new StartPurchaseOrderCommandHandler(repositoryStub);
 
-  //   const eventData: EventData<AddDraftPurchaseOrderEventData> = {
-  //     principalId: faker.datatype.uuid(),
-  //     customerId: faker.datatype.uuid(),
-  //     code: parseInt(faker.datatype.number().toString(), 10),
-  //     totalAmount: faker.datatype.float(),
-  //     discountAmount: faker.datatype.float(),
-  //     createdAt: new Date(),
-  //     timestamp: new Date().toISOString(),
-  //     eventType: 'AddDraftPurchaseOrderEvent',
-  //   };
+    const eventData: StartPurchaseOrderData = {
+      purchaseOrderId: faker.datatype.uuid(),
+      cardToken: faker.random.alphaNumeric(),
+      installments: 3,
+    };
 
-  //   try {
-  //     await handler.handle(eventData);
-  //   } catch (e: any) {
-  //     expect(e).toBeInstanceOf(EventHandlerError);
-  //     expect(e.message).toEqual('Erro ao cadastrar o pedido.');
-  //   }
-  // });
+    const result = await handler.handle(eventData);
+
+    expect(result).toBe(false);
+  });
 });
