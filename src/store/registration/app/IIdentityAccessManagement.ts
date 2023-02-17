27e9297e6578f59a7d9178987e9f94d8a35d3777
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-02-16 23:05:11 -0300

    chore: WIP

--- src/store/registration/app/IIdentityAccessManagement.ts
+++ src/store/registration/app/IIdentityAccessManagement.ts
@@ -2,8 +2,6 @@ import User from '@registration/domain/User';
 
 interface IIdentityAccessManagement {
   registerUser(user: User): Promise<any>;
-
-  changeRole(userId: string, role: string): Promise<any>;
 }
 
 export default IIdentityAccessManagement;
--- src/store/registration/infra/IAM/KeyCloakIAM.ts
+++ src/store/registration/infra/IAM/KeyCloakIAM.ts
@@ -2,12 +2,8 @@ import IIdentityAccessManagement from '@registration/app/IIdentityAccessManageme
 import User from '@registration/domain/User';
 
 // TODO: add keycloack sdk
-export default class KeyCloackIAM implements IIdentityAccessManagement {
+export default class KeyCloakIAM implements IIdentityAccessManagement {
   public async registerUser(_user: User): Promise<any> {
-    throw new Error('Method not implemented.');
-  }
-
-  public async changeRole(_userId: string, _role: string): Promise<any> {
     throw new Error('Method not implemented.');
   }
 }
