c291fe63a7aaba3d0612f08c4e1d87d0ceed5e42
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-02-24 20:50:49 -0300

    chore: add log in the errorHandler

--- src/api/middlewares/errorHandler.ts
+++ src/api/middlewares/errorHandler.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 function errorHandler(error: Error, _: Request, response: Response, __: NextFunction) {
   console.error(error.stack);
+  console.error(error.cause);
 
   return response.status(500).json({ message: 'Internal Server Error' });
 }
