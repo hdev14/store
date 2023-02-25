e742e9f49cca392de147b888516dd794c3d5ebe3
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-02-25 11:26:45 -0300

    feat(shared): ensure that BullEventQueue calls bullmq.Queue class

--- src/store/shared/BullEventQueue.ts
+++ src/store/shared/BullEventQueue.ts
@@ -1,8 +1,31 @@
+import { Queue } from 'bullmq';
 import Event from './abstractions/Event';
 import IEventQueue from './abstractions/IEventQueue';
 
-// TODO: implement with bullmq
 export default class BullEventQueue implements IEventQueue {
+  private readonly queue: Queue;
+
+  constructor() {
+    const {
+      REDIS_HOST,
+      REDIS_PORT,
+      EVENT_QUEUE_NAME,
+      EVENT_QUEUE_ATTEMPTS,
+      EVENT_QUEUE_DELAY,
+    } = process.env;
+
+    this.queue = new Queue(EVENT_QUEUE_NAME!, {
+      connection: {
+        host: REDIS_HOST,
+        port: parseInt(REDIS_PORT!, 10),
+      },
+      defaultJobOptions: {
+        attempts: parseInt(EVENT_QUEUE_ATTEMPTS!, 10),
+        backoff: { type: 'fixed', delay: parseInt(EVENT_QUEUE_DELAY!, 10) },
+      },
+    });
+  }
+
   public async enqueue(event: Event): Promise<void> {
     throw new Error('Method not implemented.');
   }
