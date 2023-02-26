40cc2a9ff82657b7f443c66010420ff668c5bdc2
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-02-26 13:48:20 -0300

    feat(sales): ensure that EventQueue.enqueue is called with RemovePurchaseOrderItemEvent

--- src/store/sales/app/commands/RemovePurchaseOrderItemCommandHandler.ts
+++ src/store/sales/app/commands/RemovePurchaseOrderItemCommandHandler.ts
@@ -1,4 +1,6 @@
+import RemovePurchaseOrderItemEvent from '@sales/domain/events/RemovePurchaseOrderItemEvent';
 import { IPurchaseOrderRepositoryCommands } from '@sales/domain/IPurchaseOrderRepository';
+import IEventQueue from '@shared/abstractions/IEventQueue';
 import IHandler from '@shared/abstractions/IHandler';
 import PurchaseOrderItemNotDeletedError from '../PurchaseOrderItemNotDeletedError.ts';
 import RemovePurchaseOrderItemCommand from './RemovePurchaseOrderItemCommand';
@@ -7,16 +9,16 @@ import RemovePurchaseOrderItemCommand from './RemovePurchaseOrderItemCommand';
 export default class RemovePurchaseOrderItemCommandHandler implements IHandler<RemovePurchaseOrderItemCommand, void> {
   constructor(
     private readonly repository: IPurchaseOrderRepositoryCommands,
+    private readonly eventQueue: IEventQueue,
   ) { }
 
   public async handle(event: RemovePurchaseOrderItemCommand): Promise<void> {
     try {
       await this.repository.deletePurchaseOrderItem(event.purchaseOrderItemId);
 
-      // this.publisher.addEvent(RemovePurchaseOrderItemEvent, {
-      //   principalId: event.purchaseOrderItemId,
-      //   timestamp: new Date().toISOString(),
-      // });
+      this.eventQueue
+        .enqueue(new RemovePurchaseOrderItemEvent(event.purchaseOrderItemId))
+        .catch(console.error.bind(console));
     } catch (e: any) {
       console.error(e.stack);
       throw new PurchaseOrderItemNotDeletedError();
