2d5983df5a7204bf66bad4f8a491ad7f4f5adbbb
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-02-26 11:56:42 -0300

    chore: comment

--- src/store/shared/abstractions/IEventQueue.ts
+++ src/store/shared/abstractions/IEventQueue.ts
@@ -1,6 +1,7 @@
 import Event from './Event';
 
 interface IEventQueue {
+  // TODO: add overload to enqueue a bulk of events
   enqueue(event: Event): Promise<void>
 
   closeConnection(): Promise<void>;
