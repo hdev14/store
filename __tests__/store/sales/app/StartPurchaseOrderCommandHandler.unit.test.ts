76ab1b5f5b956e55e82cb6c48702e0d5e07b8c59
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-02-26 14:04:22 -0300

    test(sales): create tests to ensure that EventQueue.enqueue is called with ChargePurchaseOrderEvent

--- __tests__/store/sales/app/StartPurchaseOrderCommandHandler.unit.test.ts
+++ __tests__/store/sales/app/StartPurchaseOrderCommandHandler.unit.test.ts
@@ -3,14 +3,21 @@ import StartPurchaseOrderCommand from '@sales/app/commands/StartPurchaseOrderCom
 import StartPurchaseOrderCommandHandler from '@sales/app/commands/StartPurchaseOrderCommandHandler';
 import PurchaseOrder, { PurchaseOrderStatus } from '@sales/domain/PurchaseOrder';
 import PurchaseOrderNotFoundError from '@sales/app/PurchaseOrderNotFoundError';
+import { mock } from 'jest-mock-extended';
+import IEventQueue from '@shared/abstractions/IEventQueue';
+import ChargePurchaseOrderEvent from '@shared/events/ChargePurchaseOrderEvent';
 import repositoryStub from '../../stubs/PurchaseOrderRepositoryStub';
+
+const eventQueueMock = mock<IEventQueue>();
+
+eventQueueMock.enqueue.mockImplementation(() => Promise.resolve());
 
 describe("StartPurchaseOrderCommandHandler's unit tests", () => {
   it('calls repository.getPurchaseOrderById with correct param', async () => {
     expect.assertions(1);
 
     const getPurchaseOrderByIdSpy = jest.spyOn(repositoryStub, 'getPurchaseOrderById');
-    const handler = new StartPurchaseOrderCommandHandler(repositoryStub);
+    const handler = new StartPurchaseOrderCommandHandler(repositoryStub, eventQueueMock);
 
     const command = new StartPurchaseOrderCommand(
       faker.datatype.uuid(),
@@ -29,7 +36,7 @@ describe("StartPurchaseOrderCommandHandler's unit tests", () => {
     jest.spyOn(repositoryStub, 'getPurchaseOrderById')
       .mockResolvedValueOnce(null);
 
-    const handler = new StartPurchaseOrderCommandHandler(repositoryStub);
+    const handler = new StartPurchaseOrderCommandHandler(repositoryStub, eventQueueMock);
 
     const command = new StartPurchaseOrderCommand(
       faker.datatype.uuid(),
@@ -61,7 +68,7 @@ describe("StartPurchaseOrderCommandHandler's unit tests", () => {
 
     jest.spyOn(repositoryStub, 'getPurchaseOrderById').mockResolvedValueOnce(purchaseOrder);
 
-    const handler = new StartPurchaseOrderCommandHandler(repositoryStub);
+    const handler = new StartPurchaseOrderCommandHandler(repositoryStub, eventQueueMock);
 
     const command = new StartPurchaseOrderCommand(
       faker.datatype.uuid(),
@@ -89,7 +96,7 @@ describe("StartPurchaseOrderCommandHandler's unit tests", () => {
     jest.spyOn(repositoryStub, 'getPurchaseOrderById').mockResolvedValueOnce(purchaseOrder);
     const updatePurchaseOrderSpy = jest.spyOn(repositoryStub, 'updatePurchaseOrder');
 
-    const handler = new StartPurchaseOrderCommandHandler(repositoryStub);
+    const handler = new StartPurchaseOrderCommandHandler(repositoryStub, eventQueueMock);
 
     const command = new StartPurchaseOrderCommand(
       faker.datatype.uuid(),
@@ -101,5 +108,59 @@ describe("StartPurchaseOrderCommandHandler's unit tests", () => {
 
     expect(updatePurchaseOrderSpy).toHaveBeenCalled();
     expect(updatePurchaseOrderSpy.mock.calls[0][0].status).toBe(PurchaseOrderStatus.STARTED);
+  });
+
+  it('calls EventQueue.enqueue with ChargePurchaseOrderEvent after updating the purchase order', async () => {
+    expect.assertions(1);
+
+    const purchaseOrder = new PurchaseOrder({
+      id: faker.datatype.uuid(),
+      customerId: faker.datatype.uuid(),
+      code: parseInt(faker.datatype.number().toString(), 10),
+      createdAt: new Date(),
+      voucher: null,
+      status: null,
+    });
+
+    jest.spyOn(repositoryStub, 'getPurchaseOrderById').mockResolvedValueOnce(purchaseOrder);
+
+    const handler = new StartPurchaseOrderCommandHandler(repositoryStub, eventQueueMock);
+
+    const command = new StartPurchaseOrderCommand(
+      faker.datatype.uuid(),
+      faker.random.alphaNumeric(),
+      3,
+    );
+
+    await handler.handle(command);
+
+    expect(eventQueueMock.enqueue.mock.calls[0][0]).toBeInstanceOf(ChargePurchaseOrderEvent);
+  });
+
+  it("doesn't throw a Error if EventQueue.enqueue throws a QueueError", async () => {
+    expect.assertions(1);
+
+    const purchaseOrder = new PurchaseOrder({
+      id: faker.datatype.uuid(),
+      customerId: faker.datatype.uuid(),
+      code: parseInt(faker.datatype.number().toString(), 10),
+      createdAt: new Date(),
+      voucher: null,
+      status: null,
+    });
+
+    jest.spyOn(repositoryStub, 'getPurchaseOrderById').mockResolvedValueOnce(purchaseOrder);
+
+    eventQueueMock.enqueue.mockRejectedValueOnce(new Error('test'));
+
+    const handler = new StartPurchaseOrderCommandHandler(repositoryStub, eventQueueMock);
+
+    const command = new StartPurchaseOrderCommand(
+      faker.datatype.uuid(),
+      faker.random.alphaNumeric(),
+      3,
+    );
+
+    expect(async () => handler.handle(command)).not.toThrow();
   });
 });
