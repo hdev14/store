dfaaea660e92ced470552bfba3ff46da4b534bec
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-03-01 20:27:41 -0300

    test(shared): create test to ensure that BullmqEventWorker calls EventMediator.send method

--- __tests__/store/shared/BullmqEventWorker.unit.test.ts
+++ __tests__/store/shared/BullmqEventWorker.unit.test.ts
@@ -1,1 +1,26 @@
-test.todo("BullmqEventWorker's unit tests");
+import IMediator from '@shared/abstractions/IMediator';
+import BullmqEventWorker from '@shared/BullmqEventWorker';
+import { Job } from 'bullmq';
+import { mock } from 'jest-mock-extended';
+import Event from '@shared/abstractions/Event';
+
+class EventStub extends Event {}
+
+describe("BullmqEventWorker's unit tests", () => {
+  it('calls EventMediator.send with correct event', async () => {
+    expect.assertions(1);
+
+    const eventMediatorMock = mock<IMediator>();
+    const eventStub = new EventStub();
+
+    const jobMock = mock<Job<Event, any, string>>({
+      data: eventStub,
+    });
+
+    const bullmqEventWorker = new BullmqEventWorker(eventMediatorMock);
+
+    await bullmqEventWorker.process(jobMock);
+
+    expect(eventMediatorMock.send).toHaveBeenCalledWith(eventStub);
+  });
+});
