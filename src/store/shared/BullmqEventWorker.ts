659e9b1522ab40c0313970ee6549a8a7f367727f
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-03-01 20:27:03 -0300

    feat(shared): ensure that BullmqEventWorker calls EventMediator.send method

--- src/store/shared/BullmqEventWorker.ts
+++ src/store/shared/BullmqEventWorker.ts
@@ -1,14 +1,12 @@
 import { Job, Processor } from 'bullmq';
 import IEventWorker from './abstractions/IEventWorker';
 import IMediator from './abstractions/IMediator';
+import Event from './abstractions/Event';
 
-// TODO
 export default class BullmqEventWorker implements IEventWorker<Parameters<Processor>> {
-  constructor(private readonly mediator: IMediator) {
+  constructor(private readonly eventMediator: IMediator) { }
 
-  }
-
-  public async process(job: Job<any, any, string>, token?: string | undefined): Promise<void> {
-    throw new Error('Method not implemented.');
+  public async process(job: Job<Event, any, string>, token?: string | undefined): Promise<void> {
+    await this.eventMediator.send(job.data);
   }
 }
