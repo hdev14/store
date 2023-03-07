213ebbab45d2850a9eb6cfc26393e08e4565b346
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-03-06 22:02:03 -0300

    chore(registration): WIP

--- src/store/registration/app/IIdentityAccessManagement.ts
+++ src/store/registration/app/IIdentityAccessManagement.ts
@@ -1,7 +1,14 @@
 import User from '@registration/domain/User';
 
+export type TokenPayload = {
+  token: string;
+  expiredAt: number;
+}
+
 interface IIdentityAccessManagement {
-  registerUser(user: User): Promise<any>;
+  auth(email: string, password: string): Promise<TokenPayload>;
+
+  registerUser(user: User): Promise<void>;
 }
 
 export default IIdentityAccessManagement;
