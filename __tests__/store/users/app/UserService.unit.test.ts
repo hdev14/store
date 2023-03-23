992825314c27a6c7eb74e638afec6891a84c41c5
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-03-23 20:19:19 -0300

    test(users): WIP

---
+++ __tests__/store/users/app/UserService.unit.test.ts
@@ -0,0 +1,9 @@
+describe("UserService's unit tests", () => {
+  test.todo('UserService.createUser()');
+
+  test.todo('UserService.updateUser()');
+
+  test.todo('UserService.getUser()');
+
+  test.todo('UserService.getUsers()');
+});
