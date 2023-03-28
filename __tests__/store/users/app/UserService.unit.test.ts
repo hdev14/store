8ecdc6e99e8fc9f4f8724d26b151d6a11ca34a08
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-03-27 21:27:34 -0300

    feat(users): ensure that user's data is validate in the UserService.createUser method

--- __tests__/store/users/app/UserService.unit.test.ts
+++ __tests__/store/users/app/UserService.unit.test.ts
@@ -1,5 +1,37 @@
+import { faker } from '@faker-js/faker';
+import { mock, mockClear } from 'jest-mock-extended';
+import ValidationError from '@shared/errors/ValidationError';
+import IIdentityAccessManagement from '@users/app/IIdentityAccessManagement';
+import UserService from '@users/app/UserService';
+
 describe("UserService's unit tests", () => {
-  test.todo('UserService.createUser()');
+  const IAMMock = mock<IIdentityAccessManagement>();
+  const userService = new UserService(IAMMock);
+
+  beforeEach(() => {
+    mockClear(IAMMock);
+  });
+
+  describe('UserService.createUser()', () => {
+    it('throws a ValidationError if data is invalid', async () => {
+      const data: any = {
+        name: 123,
+        email: 'invalidemail.com',
+        password: 123,
+        document: 123,
+      };
+
+      try {
+        await userService.createUser(data);
+      } catch (e: any) {
+        expect(e).toBeInstanceOf(ValidationError);
+        expect(e.errors[0].field).toEqual('name');
+        expect(e.errors[1].field).toEqual('email');
+        expect(e.errors[2].field).toEqual('document');
+        expect(e.errors[3].field).toEqual('password');
+      }
+    });
+  });
 
   test.todo('UserService.updateUser()');
 
--- src/store/users/app/UserService.ts
+++ src/store/users/app/UserService.ts
@@ -1,4 +1,5 @@
-import { UserProps } from 'src/store/users/domain/User';
+import crypto from 'crypto';
+import User, { UserProps } from '@users/domain/User';
 import IIdentityAccessManagement from './IIdentityAccessManagement';
 import IUserService, { CreateUserData, UpdateUserData } from './IUserService';
 
@@ -7,7 +8,16 @@ export default class UserService implements IUserService {
   constructor(private readonly IAM: IIdentityAccessManagement) {}
 
   public async createUser(data: CreateUserData): Promise<UserProps> {
-    throw new Error('Method not implemented.');
+    const user = new User({
+      id: crypto.randomUUID(),
+      name: data.name,
+      email: data.email,
+      document: data.document,
+      password: data.password,
+      createdAt: new Date(),
+    });
+
+    return {} as any;
   }
 
   public async updateUser(userId: string, data: UpdateUserData): Promise<UserProps> {
