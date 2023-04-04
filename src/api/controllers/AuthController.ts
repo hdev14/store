a148b79e895b5cc7040f74da37116a31b147b119
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-04-04 20:40:31 -0300

    chore(api): controllers WIP

--- src/api/controllers/AuthController.ts
+++ src/api/controllers/AuthController.ts
@@ -2,7 +2,25 @@ import { NextFunction, Request, Response } from 'express';
 
 // TODO: add implementation
 export default class AuthController {
-  public async login(request: Request, response: Response, next: NextFunction) {
+  public async auth(request: Request, response: Response, next: NextFunction) {
+    try {
+      return response.status(200).json();
+    } catch (e) {
+      // TODO: validate HttpError (401)
+      return next(e);
+    }
+  }
+
+  public async addPermission(request: Request, response: Response, next: NextFunction) {
+    try {
+      return response.status(200).json();
+    } catch (e) {
+      // TODO: validate HttpError (401)
+      return next(e);
+    }
+  }
+
+  public async removePermission(request: Request, response: Response, next: NextFunction) {
     try {
       return response.status(200).json();
     } catch (e) {
--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -9,4 +9,28 @@ export default class UserController {
       return next(e);
     }
   }
+
+  public async updateUser(_: Request, response: Response, next: NextFunction) {
+    try {
+      return response.status(204).json();
+    } catch (e) {
+      return next(e);
+    }
+  }
+
+  public async getUser(_: Request, response: Response, next: NextFunction) {
+    try {
+      return response.status(204).json();
+    } catch (e) {
+      return next(e);
+    }
+  }
+
+  public async getUsers(_: Request, response: Response, next: NextFunction) {
+    try {
+      return response.status(204).json();
+    } catch (e) {
+      return next(e);
+    }
+  }
 }
