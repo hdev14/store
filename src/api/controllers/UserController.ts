ac73058afd187eaa219e814f887b87f2ca1843fc
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-04-17 20:26:06 -0300

    feat(api): finish implementation of updateUser method

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -2,6 +2,7 @@ import HttpStatusCodes from '@api/HttpStatusCodes';
 import HttpError from '@shared/errors/HttpError';
 import ValidationError from '@shared/errors/ValidationError';
 import IUserService from '@users/app/IUserService';
+import UserNotFoundError from '@users/app/UserNotFoundError';
 import { NextFunction, Request, Response } from 'express';
 
 // TODO: add all methods
@@ -37,10 +38,34 @@ export default class UserController {
     }
   }
 
-  public async updateUser(_: Request, response: Response, next: NextFunction) {
+  public async updateUser(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.status(204).json();
+      const { id } = request.params;
+      const {
+        name, email, document, password,
+      } = request.body;
+
+      const user = await this.userService.updateUser(id, {
+        name,
+        email,
+        document,
+        password,
+      });
+
+      return response.status(200).json(user);
     } catch (e) {
+      if (e instanceof UserNotFoundError) {
+        return response.status(HttpStatusCodes.NOT_FOUND).json({
+          message: e.message,
+        });
+      }
+
+      if (e instanceof ValidationError) {
+        return response.status(HttpStatusCodes.BAD_REQUEST).json({
+          errors: e.errors,
+        });
+      }
+
       return next(e);
     }
   }
