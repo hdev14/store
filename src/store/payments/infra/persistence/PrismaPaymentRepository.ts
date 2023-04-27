9daa747f1a1fa72ccf62a311bc78350ca61c0298
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-04-26 21:22:02 -0300

    feat(payments): add try/catch

--- src/store/payments/infra/persistence/PrismaPaymentRepository.ts
+++ src/store/payments/infra/persistence/PrismaPaymentRepository.ts
@@ -3,7 +3,9 @@ import Prisma from '@shared/Prisma';
 import IPaymentRepository from '@payments/domain/IPaymentRepository';
 import Payment, { PaymentMethods, PaymentStatus } from '@payments/domain/Payment';
 import Transaction, { TransactionStatus } from '@payments/domain/Transaction';
+import RepositoryError from '@shared/errors/RepositoryError';
 
+// TODO: Finish implementation
 export default class PrismaPaymentRepository implements IPaymentRepository {
   private readonly connection: PrismaClient;
 
@@ -12,40 +14,46 @@ export default class PrismaPaymentRepository implements IPaymentRepository {
   }
 
   public async getPaymentsByPurchaseOrderId(id: string): Promise<Payment[]> {
-    const results = await this.connection.payment.findMany({
-      where: { purchaseOrderId: id },
-      include: { transactions: true },
-    });
+    try {
+      const results = await this.connection.payment.findMany({
+        where: { purchaseOrderId: id },
+        include: { transactions: true },
+      });
 
-    const payments: Payment[] = [];
+      const payments: Payment[] = [];
 
-    for (const result of results) {
-      const transactions: Transaction[] = [];
+      for (const result of results) {
+        const transactions: Transaction[] = [];
 
-      for (const resultTransaction of result.transactions) {
-        transactions.push(new Transaction({
-          id: resultTransaction.id,
-          details: resultTransaction.details,
-          externalId: resultTransaction.externalId,
-          payload: resultTransaction.payload as string,
-          paymentId: resultTransaction.paymentId,
-          registeredAt: resultTransaction.registeredAt,
-          status: resultTransaction.status as TransactionStatus,
+        for (const resultTransaction of result.transactions) {
+          transactions.push(new Transaction({
+            id: resultTransaction.id,
+            details: resultTransaction.details,
+            externalId: resultTransaction.externalId,
+            payload: resultTransaction.payload as string,
+            paymentId: resultTransaction.paymentId,
+            registeredAt: resultTransaction.registeredAt,
+            status: resultTransaction.status as TransactionStatus,
+          }));
+        }
+
+        payments.push(new Payment({
+          id: result.id,
+          gateway: result.gateway,
+          method: result.method as PaymentMethods,
+          purchaseOrderId: result.purchaseOrderId,
+          status: result.status as PaymentStatus,
+          value: result.value,
+          transactions,
         }));
       }
 
-      payments.push(new Payment({
-        id: result.id,
-        gateway: result.gateway,
-        method: result.method as PaymentMethods,
-        purchaseOrderId: result.purchaseOrderId,
-        status: result.status as PaymentStatus,
-        value: result.value,
-        transactions,
-      }));
+      return payments;
+    } catch (error) {
+      throw new RepositoryError(this.constructor.name, 'Erro ao buscar as informações dos pagamentos', {
+        cause: error,
+      });
     }
-
-    return payments;
   }
 
   public async getPaymentById(id: string): Promise<Payment> {
