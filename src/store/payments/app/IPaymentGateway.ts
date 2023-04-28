466b79cb234ee6a359926a160ccb0e2950a90eb9
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-04-27 21:12:27 -0300

    refactor(payments): change implementation of FakePaymentGateway

--- src/store/payments/app/IPaymentGateway.ts
+++ src/store/payments/app/IPaymentGateway.ts
@@ -1,25 +1,31 @@
+import { TransactionStatus } from '@payments/domain/Transaction';
 import Payment from 'src/store/payments/domain/Payment';
 
+// eslint-disable-next-line no-shadow
+export enum PaymentTypes {
+  CREDIT_CARD,
+}
+
 export type Item = {
-  productName: string;
+  id: string;
+  productId: string;
   quantity: number;
 }
 
-export type Customer = {
-  name: string;
-  email: string;
-  phone: string;
-  document: string;
-}
-
 export type MakePaymentParams = {
+  type: PaymentTypes,
+  customerId: string;
   items: Item[];
-  customer: Customer;
   payment: Payment
 }
 
+export type PaymentResult = {
+  status: TransactionStatus,
+  payload: string;
+}
+
 interface IPaymentGateway {
-  makePayment(params: MakePaymentParams): Promise<boolean>;
+  makePayment(params: MakePaymentParams): Promise<PaymentResult>;
 }
 
 export default IPaymentGateway;
--- src/store/payments/infra/payment/FakePaymentGateway.ts
+++ src/store/payments/infra/payment/FakePaymentGateway.ts
@@ -1,8 +1,17 @@
-import IPaymentGateway, { MakePaymentParams } from 'src/store/payments/app/IPaymentGateway';
+import IPaymentGateway, { MakePaymentParams, PaymentResult } from 'src/store/payments/app/IPaymentGateway';
 
 export default class FakePaymentGateway implements IPaymentGateway {
-  public async makePayment(params: MakePaymentParams): Promise<boolean> {
+  public async makePayment(params: MakePaymentParams): Promise<PaymentResult> {
     console.info(params);
-    return (Math.random() * 100) < 95;
+
+    const paymentStatus: any[] = [
+      'paid',
+      'refused',
+    ];
+
+    return Promise.resolve({
+      status: paymentStatus[parseInt((Math.random() * 1).toFixed(1), 10)],
+      payload: JSON.stringify({ fake: true }),
+    });
   }
 }
